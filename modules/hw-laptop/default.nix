69f6361f1ed83fafda84f74ce50b23e1a29a3821
l <alinkbetweennets@protonmail.com>
2023-10-11 11:26:37 +0200

    more hardware

--- modules/hw-laptop/default.nix
+++ modules/hw-laptop/default.nix
@@ -1,17 +1,24 @@
 { config, flake-self, system-config, pkgs, lib, ... }:
 with lib;
 let cfg = config.link.laptop;
-in
-{
-  options.link.laptop = {
-    enable = mkEnableOption "activate laptop";
-  };
+in {
+  options.link.laptop = { enable = mkEnableOption "activate laptop"; };
   config = mkIf cfg.enable {
     link.desktop.enable = true;
     link.hardware.enable = true;
     #options.type = "laptop";
-    networking.wireless.enable = !config.networking.networkmanager.enable;
-    services.xserver.libinput.enable = true;
+    #networking.wireless.enable = !config.networking.networkmanager.enable;
+    services.xserver.libinput.enable = link.xserver;
+    services.auto-cpufreq.enable = true; # TLP replacement
+    services.auto-cpufreq.settings = {
+      battery = {
+        governor = "powersave";
+        turbo = "never";
+      };
+      charger = {
+        governor = "performance";
+        turbo = "auto";
+      };
+    };
   };
 }
-
