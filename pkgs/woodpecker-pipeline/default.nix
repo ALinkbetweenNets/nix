e90a57c6ba80f33052a0f8b9f5aa94c389687ac9
MayNiklas <info@niklas-steffen.de>
2023-10-11 16:36:04 +0200

    improve woodpecker-pipeline

--- pkgs/woodpecker-pipeline/default.nix
+++ pkgs/woodpecker-pipeline/default.nix
@@ -9,15 +9,15 @@ writeText "pipeline" (builtins.toJSON {
         "aarch64-linux" = "linux/arm64";
         "x86_64-linux" = "linux/amd64";
       };
-      nixFlakeCheck = {
-        name = "Nix flake check";
-        image = "bash";
-        commands = [ "nix flake check" ];
-      };
       nixFlakeShow = {
         name = "Nix flake show";
         image = "bash";
         commands = [ "nix flake show" ];
+      };
+      nixFlakeCheck = {
+        name = "Nix flake check";
+        image = "bash";
+        commands = [ "nix flake check" ];
       };
       atticSetupStep = {
         name = "Setup Attic";
@@ -37,29 +37,32 @@ writeText "pipeline" (builtins.toJSON {
             # platform will be deprecated in the future!
             platform = woodpecker-platforms."${arch}";
             steps = pkgs.lib.lists.flatten
-              ([ nixFlakeCheck nixFlakeShow atticSetupStep ] ++ (map
+              ([ nixFlakeShow nixFlakeCheck atticSetupStep ] ++ (map
                 (host:
-                  # only build hosts for the arch we are currently building
-                  if (flake-self.nixosConfigurations.${host}.pkgs.stdenv.hostPlatform.system
-                    != arch) then
-                    [ ]
-                  else if
+                  if
                   # Skip hosts with this option set
                     flake-self.nixosConfigurations.${host}.config.link.options.CISkip then
+                    [ ]
+                  else if
+                  # only build hosts for the arch we are currently building
+                    (flake-self.nixosConfigurations.${host}.pkgs.stdenv.hostPlatform.system
+                      != arch) then
                     [ ]
                   else [
                     {
                       name = "Build ${host}";
                       image = "bash";
-                      commands = [
-                        "nix build '.#nixosConfigurations.${host}.config.system.build.toplevel' -o 'result-${host}'"
-                      ];
+                      commands = [ "nix build '.#nixosConfigurations.${host}.config.system.build.toplevel' -o 'result-${host}'" ];
+                    }
+                    {
+                      "name" = "Show ${host} info";
+                      "image" = "bash";
+                      "commands" = [ "nix path-info --closure-size -h $(readlink -f 'result-${host}')" ];
                     }
                     {
                       name = "Push ${host} to Attic";
                       image = "bash";
-                      commands =
-                        [ "attic push lounge-rocks:nix-cache 'result-${host}'" ];
+                      commands = [ "attic push lounge-rocks:nix-cache 'result-${host}'" ];
                     }
                   ])
                 (builtins.attrNames flake-self.nixosConfigurations)));
