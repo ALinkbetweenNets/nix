5389b107ac18ce3b4bb7a79d649a8f3655ef95f4
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-11 10:45:15 +0200

    gaming profile

--- home-manager/modules/gaming/default.nix
+++ home-manager/modules/gaming/default.nix
@@ -1,8 +1,14 @@
-{ lib, pkgs, config, flake-self, system-config, ... }: {
-  home.packages = with pkgs;
-    [
-      prismlauncher # minecraft launcher
-      steam
-    ] ++ lib.optionals
+{ lib, pkgs, config, flake-self, system-config, ... }:
+with lib;
+let cfg = config.link.gaming;
+in {
+  options.link.gaming.enable = mkEnableOption "enable gaming";
+  config = mkIf cfg.enable {
+    home.packages = with pkgs;
+      [
+        prismlauncher # minecraft launcher
+        steam
+      ] ++ lib.optionals
       (system-config.nixpkgs.hostPlatform.system == "x86_64-linux") [ ];
+  };
 }
