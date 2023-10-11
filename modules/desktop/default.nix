ecfad330f3e1031c95dada5685de24fe7194a839
l <alinkbetweennets@protonmail.com>
2023-10-11 11:55:53 +0200

    networking

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -5,6 +5,8 @@ in {
   options.link.desktop.enable = mkEnableOption "activate desktop";
   config = mkIf cfg.enable {
     link.common.enable = true;
+    link.wayland.enable = lib.mkDefault true;
+    link.xserver.enable = lib.mkDefault true;
     networking.networkmanager.enable = true;
     services.xserver.enable = true;
     services.xserver.displayManager.sddm.enable = true;
@@ -19,6 +21,18 @@ in {
       pulse.enable = true;
       #jack.enable = true;
       #media-session.enable = true;
+    };
+    networking.firewall = {
+      allowedTCPPortRanges = [{
+        from = 1714;
+        to = 1764;
+      } # KDE Connect
+        ];
+      allowedUDPPortRanges = [{
+        from = 1714;
+        to = 1764;
+      } # KDE Connect
+        ];
     };
   };
 }
