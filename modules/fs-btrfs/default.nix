3c78c43cfac1b4b99bb6a9db15cd9da311f1a559
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-11 11:26:29 +0200

    filesystem contents

--- modules/fs-btrfs/default.nix
+++ modules/fs-btrfs/default.nix
@@ -4,7 +4,10 @@ let cfg = config.link.btrfs;
 in {
   options.link.btrfs.enable = mkEnableOption "activate btrfs";
   config = mkIf cfg.enable {
-    boot.initrd.supportedFilesystems=["btrfs"];
-
+    boot.initrd.supportedFilesystems = [ "btrfs" ];
+    virtualisation.docker.storageDriver = "btrfs";
+    services.btrfs.autoScrub.enable = true;
+    services.btrfs.autoScrub.fileSystems = [ "/" ];
+    fileSystems = { "/".options = [ "compress=zstd" ]; };
   };
 }
