57fe6d4d4f0cda67f9f7f9ae95ff937620ba3f3d
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-12 17:42:06 +0200

    printing& scanning

--- modules/printing/default.nix
+++ modules/printing/default.nix
@@ -4,8 +4,27 @@ let cfg = config.link.printing;
 in {
   options.link.printing.enable = mkEnableOption "activate printing";
   config = mkIf cfg.enable {
-    # CUPS
-    services.printing.enable = true;
-    hardware.sane.enable = true;
+    services = {
+      printing = {
+        # CUPS
+        enable = true;
+        drivers = with pkgs; [ hplip ];
+      };
+      avahi = {
+        enable = true;
+        nssmdns = true;
+        # for a WiFi printer
+        openFirewall = true;
+      };
+      saned.enable = true;
+      ipp-usb.enable = true;
+    };
+    hardware.sane = {
+      enable = true;
+      openFirewall = true;
+      extraBackends = with pkgs; [ epkowa sane-airscan ];
+    };
+    users.users.l.extraGroups = [ "scanner" "lp" ];
+    environment.systemPackages = with pkgs; [ skanpage ]; # Scanner Frontend
   };
 }
