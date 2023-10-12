9d7f3e69b7f1ae0a3cd2baa5106ff9ce1e1d8cf4
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-12 17:41:44 +0200

    docker

---
+++ modules/docker/docker.nix
@@ -0,0 +1,22 @@
+{ config, flake-self, system-config, pkgs, lib, ... }:
+with lib;
+let cfg = config.link.docker;
+in {
+  options.link.docker = { enable = mkEnableOption "activate docker"; };
+  config = mkIf cfg.enable {
+    virtualisation.docker = {
+      enable = true;
+      # rootless = {
+      #   enable = true;
+      #   setSocketVariable = true;
+      # };
+    };
+    users.extraGroups.docker.members = [ "l" ];
+
+    environment.systemPackages = with pkgs; [
+      docker
+      # docker-compose
+      docker-buildx
+    ];
+  };
+}
