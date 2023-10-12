52b637a8c71c2fafb4e2fcff4df52ecf1f8732c7
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-12 16:48:40 +0200

    libvirt& virt-manager

---
+++ modules/libvirt.nix/default.nix
@@ -0,0 +1,22 @@
+{ config, system-config, pkgs, lib, ... }:
+with lib;
+let cfg = config.link.libvirt;
+in {
+  options.link.libvirt.enable = mkEnableOption "activate libvirt";
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      virtiofsd
+      libvirt
+      libguestfs
+      qemu
+      qemu-utils
+      ebtables
+    
+    ];
+
+    virtualisation.libvirtd.enable = true;
+    virtualisation.libvirtd.qemu.swtpm.enable = true;
+    virtualisation.spiceUSBRedirection.enable = true;
+    users.users.l.extraGroups = [ "libvirtd" "kvm" ];
+  };
+}
