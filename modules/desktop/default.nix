5c101c4f9ed5f2743ebaad7dea2306ca1ed3eb34
l <alinkbetweennets@protonmail.com>
2023-10-12 13:32:22 +0200

    plasma module

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -8,6 +8,7 @@ in {
       common.enable = true;
       wayland.enable = lib.mkDefault true;
       xserver.enable = lib.mkDefault true;
+      plasma.enable = lib.mkDefault true;
     };
     networking = {
       networkmanager.enable = true;
@@ -28,11 +29,6 @@ in {
     hardware.pulseaudio.enable = false;
     security.rtkit.enable = true;
     services = {
-      xserver = {
-        enable = true;
-        displayManager.sddm.enable = true;
-        desktopManager.plasma5.enable = true;
-      };
       pipewire = {
         enable = true;
         alsa.enable = true;
