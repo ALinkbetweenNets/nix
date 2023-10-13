f8f0643d942e276e0c863c89e271ee00a4999eb3
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-13 20:20:24 +0200

    in

---
+++ machines/in/configuration.nix
@@ -0,0 +1,18 @@
+{ self, ... }:
+{ pkgs, lib, config, flake-self, home-manager, ... }: {
+  imports = [
+    ./hardware-configuration.nix
+    home-manager.nixosModules.home-manager
+  ];
+  home-manager.users.l = flake-self.homeConfigurations.convertible;
+  link = {
+    convertible.enable = true;
+    main.enable = true;
+    cpu-intel.enable = true;
+  };
+  networking.hostName = "xn";
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ]; # Is this needed?
+  environment.systemPackages = with pkgs;    [ ];
+  #system.stateVersion = "23.05";
+
+}
