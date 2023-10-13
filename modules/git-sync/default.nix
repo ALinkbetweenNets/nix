4b57be9547da160fbd92c35af8dc7e63166af41c
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-13 19:04:39 +0200

    git-sync

---
+++ modules/git-sync/default.nix
@@ -0,0 +1,43 @@
+{ config, system-config, pkgs, lib, ... }:
+with lib;
+let cfg = config.link.adb;
+in {
+  options.link.adb.enable = mkEnableOption "activate adb";
+  config = mkIf cfg.enable {
+    systemd.services.git-pull-repo-obsidian = {
+      description = "Regularly pull changes from Git repository obsidian";
+      serviceConfig.Type = "oneshot";
+      serviceConfig.ExecStart = "/usr/bin/git -C /home/l/obsidian pull";
+    };
+    systemd.timers.git-pull-timer-repo-obsidian = {
+      description = "Timer to regularly pull changes from Git repository obsidian";
+      timerConfig.OnCalendar =
+        "*-*-* *:0/05:00";
+      timerConfig.Unit = "git-pull-repo-obsidian.service";
+    };
+
+    systemd.services.git-pull-repo-s = {
+      description = "Regularly pull changes from Git repository s";
+      serviceConfig.Type = "oneshot";
+      serviceConfig.ExecStart = "/usr/bin/git -C /home/l/s pull";
+    };
+    systemd.timers.git-pull-timer-repo-s = {
+      description = "Timer to regularly pull changes from Git repository s";
+      timerConfig.OnCalendar =
+        "*-*-* *:0/30:00";
+      timerConfig.Unit = "git-pull-repo-s.service";
+    };
+
+    systemd.services.git-pull-repo-nix = {
+      description = "Regularly pull changes from Git repository nix";
+      serviceConfig.Type = "oneshot";
+      serviceConfig.ExecStart = "/usr/bin/git -C /home/l/nix pull";
+    };
+    systemd.timers.git-pull-timer-repo-nix = {
+      description = "Timer to regularly pull changes from Git repository nix";
+      timerConfig.OnCalendar =
+        "*-*-* *:0/10:00";
+      timerConfig.Unit = "git-pull-repo-nix.service";
+    };
+  };
+}
