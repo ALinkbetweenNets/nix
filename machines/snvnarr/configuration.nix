a1f31438d8ee3387828f93af69e31881d2b60cd0
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-13 19:57:07 +0200

    snvnarr

---
+++ machines/snvnarr/configuration.nix
@@ -0,0 +1,12 @@
+{ self, ... }:
+{ pkgs, lib, config, flake-self, home-manager, ... }: {
+  imports = [
+    ./hardware-configuration.nix
+    home-manager.nixosModules.home-manager
+  ];
+  home-manager.users.l = flake-self.homeConfigurations.server;
+  link = {
+    arr.enable = true;
+    vm.enable = true;
+  };
+}
