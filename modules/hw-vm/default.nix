ebc8a16d54629bffb49f6ed0e78e5da85a1d6ac0
ALinkbetweenNets <alinkbetweennets@protonmail.com>
2023-10-13 19:57:53 +0200

    link.vm

---
+++ modules/hw-vm/default.nix
@@ -0,0 +1,11 @@
+{ config, flake-self, system-config, pkgs, lib, ... }:
+with lib;
+let cfg = config.link.vm;
+in {
+  options.link.vm = { enable = mkEnableOption "activate vm"; };
+  config = mkIf cfg.enable {
+    link.common.enable = true;
+  };
+  environment.systemPackages = with pkgs; [
+    virtiofsd];
+}
