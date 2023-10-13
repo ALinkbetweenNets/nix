7baeb5d344ed8b11ed9ed8172828799e9e4c831c
ALinkbetweenNets <alinkbetweennets@protonmail.com>
2023-10-13 18:39:23 +0200

    disabled kdeconnect-indicator

--- home-manager/profiles/main.nix
+++ home-manager/profiles/main.nix
@@ -9,7 +9,7 @@ with lib; {
     link.python.enable = true;
     services.kdeconnect = {
       enable = true;
-      indicator = true;
+      indicator =false;
     };
     home.packages = with pkgs; [
       alacritty
