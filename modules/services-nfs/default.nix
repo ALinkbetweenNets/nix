9197c4637e579690c47c6d62450de5c9f55f5598
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-13 10:47:13 +0200

    fixed rpcbind syntax

--- modules/services-nfs/default.nix
+++ modules/services-nfs/default.nix
@@ -4,17 +4,19 @@ let cfg = config.link.nfs;
 in {
   options.link.nfs.enable = mkEnableOption "activate nfs";
   config = mkIf cfg.enable {
-    services.  nfs.server = {
-      enable = true;
-      # hostName = "sn";
-      createMountPoints = true;
-      statdPort = 4000;
-      lockdPort = 4001;
-      mountdPort = 4002;
-      exports = ''
-        /export *(rw,no_subtree_check,insecure)
-      '';
+    services = {
+      nfs.server = {
+        enable = true;
+        # hostName = "sn";
+        createMountPoints = true;
+        statdPort = 4000;
+        lockdPort = 4001;
+        mountdPort = 4002;
+        exports = ''
+          /export *(rw,no_subtree_check,insecure)
+        '';
+      };
+      rpcbind.enable = true;
     };
-    rpcbind.enable = true;
   };
 }
