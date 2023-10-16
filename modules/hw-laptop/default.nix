f8a36c6c71ca783f5817cbe2660778269e2d119d
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-16 11:20:56 +0200

    laptop wireguard

--- modules/hw-laptop/default.nix
+++ modules/hw-laptop/default.nix
@@ -4,8 +4,13 @@ let cfg = config.link.laptop;
 in {
   options.link.laptop = { enable = mkEnableOption "activate laptop"; };
   config = mkIf cfg.enable {
-    link.desktop.enable = true;
-    link.hardware.enable = true;
+    link = {
+      desktop.enable = true;
+      hardware.enable = true;
+      wireguard.enable = true;
+      wg-fritz.enable = true;
+      wg-deep.enable = true;
+    };
     #options.type = "laptop";
     #networking.wireless.enable = !config.networking.networkmanager.enable;
     networking.networkmanager = {
@@ -13,16 +18,20 @@ in {
       wifi.macAddress = "stable";
     };
     hardware.bluetooth.enable = true;
-    services.xserver.libinput.enable = true;
-    services.auto-cpufreq.enable = true; # TLP replacement
-    services.auto-cpufreq.settings = {
-      battery = {
-        governor = "powersave";
-        turbo = "never";
-      };
-      charger = {
-        governor = "performance";
-        turbo = "auto";
+    services = {
+      xserver.libinput.enable = true;
+      auto-cpufreq = {
+        enable = true; # TLP replacement
+        settings = {
+          battery = {
+            governor = "powersave";
+            turbo = "never";
+          };
+          charger = {
+            governor = "performance";
+            turbo = "auto";
+          };
+        };
       };
     };
   };
--- modules/main/default.nix
+++ modules/main/default.nix
@@ -4,11 +4,13 @@ let cfg = config.link.main;
 in {
   options.link.main = { enable = mkEnableOption "activate main"; };
   config = mkIf cfg.enable {
-    link.desktop.enable = true;
-    link.adb.enable = true;
-    link.printing.enable = lib.mkIf config.link.office.enable true;
-    link.syncthing.enable = true;
-    link.git-sync.enable = true;
+    link = {
+      desktop.enable = true;
+      adb.enable = true;
+      printing.enable = lib.mkIf config.link.office.enable true;
+      syncthing.enable = true;
+      git-sync.enable = true;
+    };
     services.mullvad-vpn.enable = true;
     services.mullvad-vpn.package = pkgs.mullvad-vpn; # gui version
   };
