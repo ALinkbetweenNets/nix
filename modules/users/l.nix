a1fa8424bad70aa4be1622687791459a800a7c47
l <alinkbetweennets@protonmail.com>
2023-10-18 22:18:40 +0200

    openssh pub keys

--- modules/users/l.nix
+++ modules/users/l.nix
@@ -14,6 +14,11 @@ in {
         ++ lib.optionals config.networking.networkmanager.enable
         [ "networkmanager" ];
       shell = "${pkgs.zsh}/bin/zsh";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDF+rCKg9anv0pU96BL0cUcbKU8w1q75kt+JGroJcE19 l@sn"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINI74luZ3xJcgaZYHzn5DtSpYufml+SbhZQV12gWGShS l@dn"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/1eePE6/pYo4aahzcDRqbmnVdx9ikKH+93yw7M1pXH l@xn"
+      ];
     };
     nix.settings.allowed-users = [ "l" ];
   };
