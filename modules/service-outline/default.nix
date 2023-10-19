15051c33eba796d970d2c7ef439afec1b1913328
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-19 15:21:29 +0200

    minio, outline prep

--- modules/service-outline/default.nix
+++ modules/service-outline/default.nix
@@ -6,14 +6,38 @@ in {
   config = mkIf cfg.enable {
     services = {
       outline = {
-        enable = true;
-        port=3123;
-        publicUrl="https://outline.alinkbetweennets.de";
+        # enable = true;
+        # port = 3123;
+        # publicUrl = "https://outline.alinkbetweennets.de";
+        # storage = {
+        #   accessKey = "...";
+        #   secretKeyFile = "/home/l/.keys/minio-outline";
+        #   uploadBucketUrl = "https://minio.s3.alinkbetweennets.de";
+        #   uploadBucketName = "outline";
+        #   region = "eu-central-1";
+        # };
       };
       nginx.virtualHosts."outline.alinkbetweennets.de" = {
         enableACME = true;
         forceSSL = true;
         locations."/" = { proxyPass = "http://127.0.0.1:3123/"; };
+      };
+      minio = {
+        enable = true;
+        listenAddress = "127.0.0.1:9000";
+        consoleAddress = "127.0.0.1:9001";
+        region = "eu-central-1";
+
+        rootCredentialsFile = "/home/l/.keys/minio";
+
+        dataDir = [ "/rz/srv/minio/data" ];
+        # configDir = "/mnt/data/minio/config";
+      };
+    };
+    systemd.services.minio = {
+      environment = {
+        MINIO_SERVER_URL = "https://s3.alinkbetweennets.de";
+        MINIO_BROWSER_REDIRECT_URL = "https://minio.s3.alinkbetweennets.de";
       };
     };
   };
