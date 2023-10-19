e9a8ca91f255e7b10f3e1acbf32927a449c5338e
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-19 15:45:47 +0200

    snvnarr config

--- machines/snvnarr/configuration.nix
+++ machines/snvnarr/configuration.nix
@@ -7,8 +7,12 @@
   home-manager.users.l = flake-self.homeConfigurations.server;
   link = {
     # arr.enable = true;
+    systemd-boot.enable = false;
+    grub.enable = true;
     vm.enable = true;
   };
+  networking.hostName = "snvnarr";
+  boot.loader.grub.device = "/dev/vda";
   services.mullvad-vpn.enable = true;
   services.mullvad-vpn.package = pkgs.mullvad-vpn; # gui version
 }
