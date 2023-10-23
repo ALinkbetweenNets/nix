39e49034ae35cdaec743afca24e63450c9e00274
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-23 15:39:50 +0200

    vscode settings, fmt

--- machines/in/hardware-configuration.nix
+++ machines/in/hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "usb_storage" "sd_mod" "sdhci_pci" ];
@@ -14,18 +15,19 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/fd15e5df-567b-43de-8426-bfb4269c7a86";
+    {
+      device = "/dev/disk/by-uuid/fd15e5df-567b-43de-8426-bfb4269c7a86";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/2AFD-1839";
+    {
+      device = "/dev/disk/by-uuid/2AFD-1839";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/8517ceca-5e11-4a47-984f-85582b244546"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/8517ceca-5e11-4a47-984f-85582b244546"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
