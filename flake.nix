38f1bb142a3471640ca7cec33b26e35138b8ec27
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-24 21:45:44 +0200

    nix flake disko

--- flake.nix
+++ flake.nix
@@ -7,6 +7,9 @@
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nur.url = "github:nix-community/NUR";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+
 
     home-manager = {
       url = "github:nix-community/home-manager";
