95b2ea7c840ee1024615ceb6acb20dc49cf5e9c7
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-25 18:11:48 +0200

    user initial hashed pw

--- modules/users/l.nix
+++ modules/users/l.nix
@@ -19,6 +19,7 @@ in {
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINI74luZ3xJcgaZYHzn5DtSpYufml+SbhZQV12gWGShS l@dn"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/1eePE6/pYo4aahzcDRqbmnVdx9ikKH+93yw7M1pXH l@xn"
       ];
+      hashedPassword = "$6$rounds=1000000$yacdewvoirbidab$4nws4XniUU045W/KXkOpKd390HfXFfYU0wkBWy3zAMNTrg22R4eyyGq8QYzIWor3w4Uf.DT61AdBfl77DHZDu0";
     };
     nix.settings.allowed-users = [ "l" ];
   };
