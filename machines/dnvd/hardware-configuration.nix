d7fc6ad1fc6ab2085982e831d802e42d02f8c7e3
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-28 18:13:43 +0200

    boot.swraid for dnvd, deepserver

--- machines/dnvd/hardware-configuration.nix
+++ machines/dnvd/hardware-configuration.nix
@@ -17,7 +17,7 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
-
+  boot.swraid.enable = true;
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
