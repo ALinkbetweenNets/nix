486056881ae62b6e294866ef17170595ce3b0b51
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-28 15:10:35 +0200

    dnvnd

---
+++ machines/dnvd/configuration.nix
@@ -0,0 +1,19 @@
+{ self, ... }:
+{ pkgs, lib, config, flake-self, home-manager, ... }: {
+  imports = [
+    ./hardware-configuration.nix
+    home-manager.nixosModules.home-manager
+  ];
+  home-manager.users.l = flake-self.homeConfigurations.server;
+  link = {
+    common.enable = true;
+    grub.enable = true;
+    systemd-boot.enable = false;
+    hardware.enable = true;
+    cpu-amd.enable = true;
+  };
+  boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
+  boot.loader.grub.devices = [ "/dev/vda" ];
+  security.sudo.wheelNeedsPassword = true;
+  networking.hostName = "dnvnd";
+}
