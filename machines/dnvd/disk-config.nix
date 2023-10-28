0db437ec74d5afa49e3aa62bf953701fc9cd27bf
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-28 18:14:05 +0200

    dnvd raid 1

--- machines/dnvd/disk-config.nix
+++ machines/dnvd/disk-config.nix
@@ -1,9 +1,37 @@
 {
   disko.devices = {
     disk = {
-      vdb = {
-        device = "/dev/vda";
+      one = {
         type = "disk";
+        device = "/dev/vda";
+        content = {
+          type = "gpt";
+          partitions = {
+            BOOT = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
+            ESP = {
+              size = "500M";
+              type = "EF00";
+              content = {
+                type = "mdraid";
+                name = "boot";
+              };
+            };
+            mdadm = {
+              size = "100%";
+              content = {
+                type = "mdraid";
+                name = "raid1";
+              };
+            };
+          };
+        };
+      };
+      two = {
+        type = "disk";
+        device = "/dev/vdb";
         content = {
           type = "gpt";
           partitions = {
@@ -12,21 +40,46 @@
               type = "EF02"; # for grub MBR
             };
             ESP = {
-              type = "EF00";
               size = "500M";
+              type = "EF00";
               content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
+                type = "mdraid";
+                name = "boot";
               };
             };
-            root = {
+            mdadm = {
               size = "100%";
               content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
+                type = "mdraid";
+                name = "raid1";
               };
+            };
+          };
+        };
+      };
+    };
+    mdadm = {
+      boot = {
+        type = "mdadm";
+        level = 1;
+        metadata = "1.0";
+        content = {
+          type = "filesystem";
+          format = "vfat";
+          mountpoint = "/boot";
+        };
+      };
+      raid1 = {
+        type = "mdadm";
+        level = 1;
+        content = {
+          type = "gpt";
+          partitions.primary = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
             };
           };
         };
