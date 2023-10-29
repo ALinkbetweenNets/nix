339e0c11d50763fdd42d548d8e56b87d73cffbb2
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-29 09:41:46 +0100

    outline, minio

--- modules/services-outline/default.nix
+++ modules/services-outline/default.nix
@@ -34,6 +34,60 @@ in {
         forceSSL = true;
         locations."/" = { proxyPass = "http://127.0.0.1:3123"; };
       };
+      nginx.virtualHosts."s3.alinkbetweennets.de" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:9000";
+          extraConfig = ''
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            # proxy_set_header Host $host;
+            proxy_connect_timeout 300;
+            # Default is HTTP/1, keepalive is only enabled in HTTP/1.1
+            proxy_http_version 1.1;
+            proxy_set_header Connection "";
+            chunked_transfer_encoding off;
+          '';
+        };
+        extraConfig = ''
+          # To allow special characters in headers
+          ignore_invalid_headers off;
+          # Allow any size file to be uploaded.
+          # Set to a value such as 1000m; to restrict file size to a specific value
+          client_max_body_size 0;
+          # To disable buffering
+          proxy_buffering off;
+        '';
+      };
+      nginx.virtualHosts."minio.s3.alinkbetweennets.de" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:9000";
+          extraConfig = ''
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            # proxy_set_header Host $host;
+            proxy_connect_timeout 300;
+            # Default is HTTP/1, keepalive is only enabled in HTTP/1.1
+            proxy_http_version 1.1;
+            proxy_set_header Connection "";
+            chunked_transfer_encoding off;
+          '';
+        };
+        extraConfig = ''
+          # To allow special characters in headers
+          ignore_invalid_headers off;
+          # Allow any size file to be uploaded.
+          # Set to a value such as 1000m; to restrict file size to a specific value
+          client_max_body_size 0;
+          # To disable buffering
+          proxy_buffering off;
+        '';
+      };
       minio = {
         enable = true;
         listenAddress = "127.0.0.1:9000";
@@ -50,6 +104,14 @@ in {
       environment = {
         MINIO_SERVER_URL = "https://s3.alinkbetweennets.de";
         MINIO_BROWSER_REDIRECT_URL = "https://minio.s3.alinkbetweennets.de";
+      };
+    };
+    networking = {
+      firewall.checkReversePath = "loose";
+      nameservers = [ "100.100.100.100" "1.1.1.1" ];
+      firewall = {
+        # minio load ballancer should have ports 80 & 443 open
+        allowedTCPPorts = [ 80 443 ];
       };
     };
   };
