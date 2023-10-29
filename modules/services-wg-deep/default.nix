eaba97cd5a019da0b7261b0c7cc20858b317de43
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-29 13:36:15 +0100

    wg-deep iptables

--- modules/services-wg-deep/default.nix
+++ modules/services-wg-deep/default.nix
@@ -27,20 +27,20 @@ in {
         privateKeyFile = "/home/l/.keys/wg-deep.private";
 
         # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-        # postUp = ''
-        #   ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
-        #   ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
-        #   ${pkgs.iptables}/bin/ip6tables -A FORWARD -i wg0 -j ACCEPT
-        #   ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
-        # '';
+        postUp = ''
+          ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
+          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
+          ${pkgs.iptables}/bin/ip6tables -A FORWARD -i wg0 -j ACCEPT
+          ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
+        '';
 
-        # # Undo the above
-        # preDown = ''
-        #   ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
-        #   ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
-        #   ${pkgs.iptables}/bin/ip6tables -D FORWARD -i wg0 -j ACCEPT
-        #   ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
-        # '';
+        # Undo the above
+        preDown = ''
+          ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
+          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
+          ${pkgs.iptables}/bin/ip6tables -D FORWARD -i wg0 -j ACCEPT
+          ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
+        '';
 
         peers = [
           {
