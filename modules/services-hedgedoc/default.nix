76e9d8cbbfdf8b92a64865b036e13f89bd5b44a3
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-29 15:04:45 +0100

    hedgedoc

---
+++ modules/services-hedgedoc/default.nix
@@ -0,0 +1,32 @@
+{ config, system-config, pkgs, lib, ... }:
+with lib;
+let cfg = config.link.hedgedoc;
+in {
+  options.link.hedgedoc.enable = mkEnableOption "activate hedgedoc";
+  config = mkIf cfg.enable {
+    services = {
+      hedgedoc = {
+        enable = true;
+        workDir = "${config.link.storage}hedgedoc";
+        settings = {
+          domain = "hedgedoc.${config.link.domain}";
+          port = 3400;
+          protocolUseSSL = true;
+          useSSL = false;
+          db = {
+            dialect = "sqlite";
+            storage = "${config.link.storage}hedgedoc/db.hedgedoc.sqlite";
+          };
+        };
+      };
+      nginx.virtualHosts."hedgedoc.${config.link.domain}" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "127.0.0.1:${config.services.hedgedoc.settings.port}";
+        };
+      };
+    };
+
+  };
+}
