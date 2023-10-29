455b6ea7308e80dd24e6b6dd1fe84781d73c36eb
l <alinkbetweennets@protonmail.com>
2023-10-29 13:33:40 +0100

    link service configurations

---
+++ modules/link/default.nix
@@ -0,0 +1,20 @@
+{ lib, pkgs, config, nixpkgs, flake-self, home-manager, ... }: with lib;
+{
+  options.link = {
+    domain = mkOption {
+      type = types.str;
+      default = "alinkbetweennets.de";
+      description = "Main Domain name for all services";
+    };
+    storage = mkOption {
+      type = types.str;
+      default = "/var/lib/";
+      description = "storage path for all services";
+    };
+    secrets = mkOption {
+      type = types.str;
+      default = "/pwd/";
+      description = "storage path for all secrets";
+    };
+  };
+}
