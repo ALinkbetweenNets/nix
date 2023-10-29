25232ff1608f99d5c7dccbe01d07a1fa26629fb5
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-29 16:51:07 +0100

    rename module folders

--- modules/fs-zfs/default.nix
+++ modules/fs-zfs/default.nix
@@ -1,8 +1,8 @@
 { lib, pkgs, config, ... }:
 with lib;
-let cfg = config.link.zfs;
+let cfg = config.link.fs.zfs;
 in {
-  options.link.zfs.enable = mkEnableOption "activate zfs";
+  options.link.fs.zfs.enable = mkEnableOption "activate zfs";
   config = mkIf cfg.enable {
     boot.loader.grub.zfsSupport = true;
     services.zfs.autoScrub.enable = true;
--- modules/users/root.nix
+++ modules/users/root.nix
@@ -6,9 +6,9 @@ in {
   config = mkIf cfg.enable {
     users.users.root = {
       openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/1eePE6/pYo4aahzcDRqbmnVdx9ikKH+93yw7M1pXH l@xn"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDF+rCKg9anv0pU96BL0cUcbKU8w1q75kt+JGroJcE19 l@sn"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINI74luZ3xJcgaZYHzn5DtSpYufml+SbhZQV12gWGShS l@dn"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/1eePE6/pYo4aahzcDRqbmnVdx9ikKH+93yw7M1pXH l@xn"
       ];
       hashedPassword = "$6$rounds=1000000$yacdewvoirbidab$4nws4XniUU045W/KXkOpKd390HfXFfYU0wkBWy3zAMNTrg22R4eyyGq8QYzIWor3w4Uf.DT61AdBfl77DHZDu0";
     };
