a353a99b41cc68a47be651594db2937a4324b300
l <alinkbetweennets@protonmail.com>
2023-10-29 08:05:50 +0100

    keycloak

---
+++ modules/services-keycloak/default.nix
@@ -0,0 +1,41 @@
+{ config, system-config, pkgs, lib, ... }:
+with lib;
+let cfg = config.link.nextcloud;
+in {
+  options.link.nextcloud.enable = mkEnableOption "activate nextcloud";
+  config = mkIf cfg.enable {
+    services = {
+      keycloak = {
+        enable = true;
+        database = {
+          passwordFile = "/pwd/keycloak";
+        };
+        settings = {
+          hostname = "auth.alinkbetweennets.de";
+          http-relative-path = "";
+          hostname-strict-backchannel = true;
+          http-port = 31123;
+          http-host = "127.0.0.1";
+          proxy = "edge";
+        };
+      };
+
+      nginx.virtualHosts = {
+        "auth.alinkbetweennets.de" = {
+          enableACME = true;
+          forceSSL = true;
+          locations = {
+            "/" = {
+              proxyPass = "http://127.0.0.1:${toString services.keycloak.settings.http-port}";
+              extraConfig = ''
+                proxy_set_header X-Forwarded-Host $http_host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-Proto $scheme;
+              '';
+            };
+          };
+        };
+      };
+    };
+  };
+}
