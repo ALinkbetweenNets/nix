5372a9c153b7bddbc387ae786363500e68c73c94
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-29 15:23:17 +0100

    outline

--- modules/services-outline/default.nix
+++ modules/services-outline/default.nix
@@ -12,107 +12,60 @@ in {
         storage = {
           accessKey = "outline";
           secretKeyFile = "${config.link.secrets}minio-outline";
-          uploadBucketUrl = "https://minio.s3.alinkbetweennets.de";
+          uploadBucketUrl = "https://minio.s3.${config.link.domain}";
           uploadBucketName = "outline";
           region = "eu-central-1";
         };
         oidcAuthentication = {
           # Parts taken from
           # http://dex.localhost/.well-known/openid-configuration
-          authUrl = "https://gitea.alinkbetweennets.de/login/oauth/authorize";
-          tokenUrl = "https://gitea.alinkbetweennets.de/login/oauth/access_token";
-          userinfoUrl = "https://gitea.alinkbetweennets.de/login/oauth/userinfo";
+          authUrl = "http://dex.localhost/auth";
+          tokenUrl = "http://dex.localhost/token";
+          userinfoUrl = "http://dex.localhost/userinfo";
           clientId = "outline";
-          clientSecretFile = ".keys/";
+          clientSecretFile = (builtins.elemAt config.services.dex.settings.staticClients 0).secretFile;
           scopes = [ "openid" "email" "profile" ];
-          usernameClaim = "l";
-          displayName = "Gitea";
+          usernameClaim = "preferred_username";
+          displayName = "Dex";
         };
       };
-      nginx.virtualHosts."outline.alinkbetweennets.de" = {
-        enableACME = true;
-        forceSSL = true;
-        locations."/" = { proxyPass = "http://127.0.0.1:3123"; };
-      };
-      nginx.virtualHosts."s3.alinkbetweennets.de" = {
-        enableACME = true;
-        forceSSL = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:9000";
-          extraConfig = ''
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            # proxy_set_header Host $host;
-            proxy_connect_timeout 300;
-            # Default is HTTP/1, keepalive is only enabled in HTTP/1.1
-            proxy_http_version 1.1;
-            proxy_set_header Connection "";
-            chunked_transfer_encoding off;
-          '';
-        };
-        extraConfig = ''
-          # To allow special characters in headers
-          ignore_invalid_headers off;
-          # Allow any size file to be uploaded.
-          # Set to a value such as 1000m; to restrict file size to a specific value
-          client_max_body_size 0;
-          # To disable buffering
-          proxy_buffering off;
-        '';
-      };
-      nginx.virtualHosts."minio.s3.alinkbetweennets.de" = {
-        enableACME = true;
-        forceSSL = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:9000";
-          extraConfig = ''
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            # proxy_set_header Host $host;
-            proxy_connect_timeout 300;
-            # Default is HTTP/1, keepalive is only enabled in HTTP/1.1
-            proxy_http_version 1.1;
-            proxy_set_header Connection "";
-            chunked_transfer_encoding off;
-          '';
-        };
-        extraConfig = ''
-          # To allow special characters in headers
-          ignore_invalid_headers off;
-          # Allow any size file to be uploaded.
-          # Set to a value such as 1000m; to restrict file size to a specific value
-          client_max_body_size 0;
-          # To disable buffering
-          proxy_buffering off;
-        '';
-      };
-      minio = {
+
+      dex = {
         enable = true;
-        listenAddress = "127.0.0.1:9000";
-        consoleAddress = "127.0.0.1:9001";
-        region = "eu-central-1";
+        settings = {
+          issuer = "https://dex.${config.link.domain}";
+          storage = {
+            type = "sqlite3";
+            config.file = "${config.link.storage}dex/db.sqlite3";
+          };
+          web.http = "127.0.0.1:5556";
+          enablePasswordDB = true;
+          staticClients = [
+            {
+              id = "outline";
+              name = "Outline Client";
+              redirectURIs = [ "http://localhost:3000/auth/oidc.callback" ];
+              secretFile = "${config.link.secrets}outline-client-secret";
+            }
+          ];
+        };
+      };
 
-        rootCredentialsFile = "/home/l/.keys/minio-outline";
-
-        dataDir = [ "/rz/srv/minio/data" ];
-        # configDir = "/mnt/data/minio/config";
+      nginx.virtualHosts."outline.${config.link.domain}" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "${config.services.outline.publicUrl}";
+        };
+      };
+      nginx.virtualHosts."dex.${config.link.domain}" = {
+        locations."/" = {
+          proxyPass = "http://${config.services.dex.settings.web.http}";
+        };
       };
     };
-    systemd.services.minio = {
-      environment = {
-        MINIO_SERVER_URL = "https://s3.alinkbetweennets.de";
-        MINIO_BROWSER_REDIRECT_URL = "https://minio.s3.alinkbetweennets.de";
-      };
-    };
-    networking = {
-      firewall.checkReversePath = "loose";
-      nameservers = [ "100.100.100.100" "1.1.1.1" ];
-      firewall = {
-        # minio load ballancer should have ports 80 & 443 open
-        allowedTCPPorts = [ 80 443 ];
-      };
+    systemd.services.dex = {
+      serviceConfig.StateDirectory = "dex";
     };
   };
 }
