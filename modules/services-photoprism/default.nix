02105cdeb33a3505e75bcbbc0c59a6325d3b2366
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-10-31 09:38:11 +0100

    photoprism

--- modules/services-photoprism/default.nix
+++ modules/services-photoprism/default.nix
@@ -10,18 +10,46 @@ in {
         originalsPath = "${config.link.storage}/photoprism/data";
         storagePath = "${config.link.storage}/photoprism/storage";
         settings = {
-          PHOTOPRISM_ADMIN_USER = "l";
+          PHOTOPRISM_ADMIN_USER = "lad";
+          PHOTOPRISM_ADMIN_PASSWORD = "GzJ&<8C@RD]Cv]Y3DF5VgTAfY543q}M:{S7h?BoIhu[_j#P0B5"; # only initial
           PHOTOPRISM_DEFAULT_LOCALE = "de";
-          PHOTOPRISM_SITE_URL = "https://photoprism.${config.link.domain}";
+          PHOTOPRISM_SITE_URL = "https://pics.${config.link.domain}";
+          PHOTOPRISM_DATABASE_DRIVER = "mysql";
+          PHOTOPRISM_DATABASE_NAME = "photoprism";
+          PHOTOPRISM_DATABASE_SERVER = "/run/mysqld/mysqld.sock";
+          PHOTOPRISM_DATABASE_USER = "photoprism";
+          PHOTOPRISM_SITE_TITLE = "Pics";
         };
         port = 2342;
         passwordFile = "${config.link.secrets}/photoprism";
-        address = "0.0.0.0";
+        address = "127.0.0.1";
       };
-      nginx.virtualHosts."photoprism.${config.link.domain}" = {
+      mysql = {
+        enable = true;
+        dataDir = "${config.link.storage}/photoprism/mysql";
+        package = pkgs.mariadb;
+        ensureDatabases = [ "photoprism" ];
+        ensureUsers = [{
+          name = "photoprism";
+          ensurePermissions = {
+            "photoprism.*" = "ALL PRIVILEGES";
+          };
+        }];
+      };
+      nginx.virtualHosts."pics.${config.link.domain}" = {
         enableACME = true;
         forceSSL = true;
-        locations."/" = { proxyPass = "http://127.0.0.1:2342/"; };
+        http2 = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${toString config.services.photoprism.port}/";
+          proxyWebsockets = true;
+          extraConfig = ''
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header Host $host;
+            proxy_buffering off;
+            proxy_http_version 1.1;
+          '';
+        };
       };
     };
   };
