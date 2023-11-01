b68a25114887cc7c755be3767f27a5fc6369eb99
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-11-01 22:27:57 +0100

    mime firefox

--- home-manager/modules/xdg/default.nix
+++ home-manager/modules/xdg/default.nix
@@ -38,6 +38,17 @@ with lib;
       "video/webm" = [ "vlc.desktop" ];
       "x-scheme-handler/sgnl" = [ "signal-desktop.desktop" ];
       "x-scheme-handler/signalcaptcha" = [ "signal-desktop.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+      "x-scheme-handler/chrome" = [ "firefox.desktop" ];
+      "application/x-extension-htm" = [ "firefox.desktop" ];
+      "application/x-extension-html" = [ "firefox.desktop" ];
+      "application/x-extension-shtml" = [ "firefox.desktop" ];
+      "application/x-extension-xhtml" = [ "firefox.desktop" ];
+      "application/x-extension-xht" = [ "firefox.desktop" ];
+      "application/xhtml+xml" = [ "firefox.desktop" ];
+
     };
   };
 }
