7d2e2981da1f912fab19549247dbe156f603a25c
l <alinkbetweennets@protonmail.com>
2023-11-01 18:54:46 +0100

    nginx hardening

--- modules/services-nginx/default.nix
+++ modules/services-nginx/default.nix
@@ -15,7 +15,6 @@ in {
       4000
       4001
       4002
-      20048 # nfs
     ];
     networking.firewall.allowedUDPPorts = [ 111 2049 4000 4001 4002 20048 ]; # nfs
     services.
@@ -28,6 +27,31 @@ in {
       logError = "stderr debug";
       package = pkgs.nginxStable.override { openssl = pkgs.libressl; };
       clientMaxBodySize = "1000m";
+
+      sslCiphers = "AES256+EECDH:AES256+EDH:!aNULL";
+      appendHttpConfig = ''
+        # Add HSTS header with preloading to HTTPS requests.
+        # Adding this header to HTTP requests is discouraged
+        map $scheme $hsts_header {
+            https   "max-age=31536000; includeSubdomains; preload";
+        }
+        add_header Strict-Transport-Security $hsts_header;
+
+        # Enable CSP for your services.
+        #add_header Content-Security-Policy "script-src 'self'; object-src 'none'; base-uri 'none';" always;
+
+        # Minimize information leaked to other domains
+        add_header 'Referrer-Policy' 'origin-when-cross-origin';
+
+        # Disable embedding as a frame
+        add_header X-Frame-Options DENY;
+
+        # Prevent injection of code in other mime types (XSS Attacks)
+        add_header X-Content-Type-Options nosniff;
+
+        # This might create errors
+        proxy_cookie_path / "/; secure; HttpOnly; SameSite=strict";
+      '';
     };
     security.acme = {
       acceptTerms = true;
