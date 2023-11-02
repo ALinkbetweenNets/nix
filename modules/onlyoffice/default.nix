1bfc4e99a9b4fcbf246a05125699fb3a49401beb
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-11-02 01:16:22 +0100

    onlyoffice

---
+++ modules/onlyoffice/default.nix
@@ -0,0 +1,16 @@
+{ config, system-config, pkgs, lib, ... }:
+with lib;
+let cfg = config.link.onlyoffice;
+in {
+  options.link.onlyoffice.enable = mkEnableOption "activate onlyoffice";
+  config = mkIf cfg.enable {
+    services = {
+      onlyoffice={
+        enable=true;
+        hostname="office.${config.link.domain}";
+        port=8000;
+
+      };
+    };
+  };
+}
