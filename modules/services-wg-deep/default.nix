971b9e4b5ffdf535e55991a947cfe26d84b8b4a3
ALinkbetweenNets <alinkbetweennets@protonmail.com>
2023-11-02 01:16:53 +0100

    wg-quick -> wireguard

--- modules/services-wg-deep/default.nix
+++ modules/services-wg-deep/default.nix
@@ -5,29 +5,24 @@ in {
   options.link.services.wg-deep.enable = mkEnableOption "activate wg-deep";
   config = mkIf cfg.enable {
     networking.nat.internalInterfaces = [ "wg-deep" ];
-    networking.wg-quick.interfaces = {
+    networking.wireguard.interfaces = {
       wg-deep = {
         # Determines the IP/IPv6 address and subnet of the client's end of the tunnel interface
-        address = [ "10.0.0.1/24" "fdc9:281f:04d7:9ee9::1/64" ];
+        ips = [ "10.0.0.1/24" "fdc9:281f:04d7:9ee9::1/64" ];
         # The port that WireGuard listens to - recommended that this be changed from default
         listenPort = 51820;
         # Path to the server's private key
         privateKeyFile = "/home/l/.keys/wg-deep.private";
 
         # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-        postUp = ''
-          ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
-          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
-          ${pkgs.iptables}/bin/ip6tables -A FORWARD -i wg0 -j ACCEPT
-          ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
+        # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
+        postSetup = ''
+          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
         '';
 
-        # Undo the above
-        preDown = ''
-          ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
-          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
-          ${pkgs.iptables}/bin/ip6tables -D FORWARD -i wg0 -j ACCEPT
-          ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
+        # This undoes the above command
+        postShutdown = ''
+          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
         '';
 
         peers = [
