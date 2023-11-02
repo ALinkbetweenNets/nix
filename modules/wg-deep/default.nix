c71cbffb238a8060373e07a804ebf73a59bbef8b
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-11-02 01:16:53 +0100

    wg-quick -> wireguard

--- modules/wg-deep/default.nix
+++ modules/wg-deep/default.nix
@@ -4,15 +4,18 @@ let cfg = config.link.wg-deep;
 in {
   options.link.wg-deep.enable = mkEnableOption "activate wg-deep";
   config = mkIf cfg.enable {
-    networking.extraHosts =
-      ''
-        10.0.0.1 deepserver.org
-        10.0.0.1 jitsi.deepserver.org
-      '';
-    networking.wg-quick.interfaces = {
+    # networking.extraHosts =
+    #   ''
+    #     10.0.0.1 deepserver.org
+    #     10.0.0.1 jitsi.deepserver.org
+    #   '';
+    networking.firewall.allowedUDPPorts = [ 51820 ];
+    networking.firewall.checkReversePath =mkForce false;
+    networking.wireguard.interfaces = {
       wg-deep = {
-        address = [ "10.0.0.2/24" "fdc9:281f:04d7:9ee9::2/64" ];
-        dns = [ "10.0.0.1" "fdc9:281f:04d7:9ee9::1" ];
+        ips = [ "10.0.0.2/24" "fdc9:281f:04d7:9ee9::2/64" ];
+        listenPort = 51820;
+        # dns = [ "10.0.0.1" "fdc9:281f:04d7:9ee9::1" ];
         privateKeyFile = "${config.link.secrets}/wg-deep-l.private";
         peers = [
           {
