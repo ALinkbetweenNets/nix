9a0a0c740868edecb3e996efa2b1281d6efe9880
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-11-10 15:11:14 +0100

    matrix, link.service-interface, deepserver interface correction

--- machines/deepserver/configuration.nix
+++ machines/deepserver/configuration.nix
@@ -14,17 +14,20 @@
     users = { jucknath.enable = true; paul.enable = true; };
     wireguard.enable = true;
     services.wg-deep.enable = true;
+    services.matrix = { enable = true; expose = false; };
     domain = "deepserver.org";
     service-ip = "10.0.0.1";
     jitsi.enable = true;
+    eth = "eth0";
+    service-interface = "wg-deep";
   };
   services.openssh.settings.PermitRootLogin = lib.mkForce "prohibit-password";
   boot.loader.grub.devices = [ "/dev/sda" "/dev/sdb" ];
   security.sudo.wheelNeedsPassword = true;
   networking = {
     hostName = "deepserver";
-    domain = "de";
-    nat.externalInterface = "enp41s0";
+    domain = "org";
+    nat.externalInterface = config.link.eth; # used by wireguard
     firewall = {
       allowedTCPPorts = [ 80 443 ];
     };
