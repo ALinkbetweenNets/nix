0fd16ca9df1f4c7bf147dc393bfaa8f59e370c6a
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-11-11 23:41:14 +0100

    containers

---
+++ modules/podman/default.nix
@@ -0,0 +1,24 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.link.podman;
+in {
+  options.link.podman.enable = mkEnableOption "activate podman containers";
+  config = mkIf cfg.enable {
+    virtualisation = {
+      oci-containers.backend = "podman";
+      podman = {
+        enable = true;
+
+        # Create a `docker` alias for podman, to use it as a drop-in replacement
+        dockerCompat = true;
+
+        # Required for containers under podman-compose to be able to talk to each other.
+        defaultNetwork.settings.dns_enabled = true;
+        # For Nixos version > 22.11
+        #defaultNetwork.settings = {
+        #  dns_enabled = true;
+        #};
+      };
+    };
+  };
+}
