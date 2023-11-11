4dd146cfe426d5148f054bbf89b5ef3fc06f70d6
l <alinkbetweennets@protonmail.com>
2023-11-11 14:12:08 +0100

    nextcloud deepserver, options

--- modules/services-nextcloud/default.nix
+++ modules/services-nextcloud/default.nix
@@ -1,13 +1,25 @@
 { config, system-config, pkgs, lib, ... }:
 with lib;
-let cfg = config.link.nextcloud;
+let cfg = config.link.services.nextcloud;
 in {
-  options.link.nextcloud.enable = mkEnableOption "activate nextcloud";
+  options.link.services.nextcloud = {
+    enable = mkEnableOption "activate matrix";
+    expose = mkOption {
+      type = types.bool;
+      default = config.link.expose;
+      description = "expose matrix to the internet with NGINX and ACME";
+    };
+    nginx = mkOption {
+      type = types.bool;
+      default = config.link.nginx.enable;
+      description = "Use service with NGINX";
+    };
+  };
   config = mkIf cfg.enable {
     services = {
       nextcloud = {
         enable = true;
-        hostName = if config.link.nginx.enable then "nextcloud.${config.link.domain}" else config.networking.hostName;
+        hostName = if cfg.nginx then "nextcloud.${config.link.domain}" else config.link.service-ip;
         config = {
           adminuser = "l";
           adminpassFile = "${config.link.secrets}/nextcloud";
@@ -29,7 +41,7 @@ in {
         database.createLocally = true;
         home = "${config.link.storage}/nextcloud";
       };
-      nginx.virtualHosts."nextcloud.${config.link.domain}" = mkIf config.link.nginx.enable {
+      nginx.virtualHosts."nextcloud.${config.link.domain}" = mkIf cfg.nginx {
         enableACME = true;
         forceSSL = true;
         #locations."/" = {
