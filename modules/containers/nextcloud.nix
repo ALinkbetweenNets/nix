0b588bbdd67e781aa767d8a44e6bec6a650bf0b9
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-11-11 23:41:14 +0100

    containers

---
+++ modules/containers/nextcloud.nix
@@ -0,0 +1,20 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.link.containers.nextcloud;
+in
+{
+  options.link.containers.nextcloud = {
+    enable = mkEnableOption "activate nextcloud container";
+  };
+  config = mkIf cfg.enable {
+    virtualisation.oci-containers.containers.nextcloud = {
+      image = "nextcloud/all-in-one";
+      # init = true;
+      autoStart = true;
+      # container_name = "nextcloud-aio-mastercontainer";
+      volumes = [ "nextcloud_aio_mastercontainer=/mnt/docker-aio-config" "/var/run/docker.sock=/var/run/docker.sock=ro" ];
+      ports = [ "80:80" "8080:8080" "8443:8443" ];
+      extraOptions = [ "/docker/nextcloud/data:/app/data:rw"  ];
+    };
+  };
+}
