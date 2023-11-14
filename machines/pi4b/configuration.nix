10e23563b37e8dc79ad3227460be3a86ae502bf0
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-11-14 20:33:21 +0100

    root home-manager

--- machines/pi4b/configuration.nix
+++ machines/pi4b/configuration.nix
@@ -34,6 +34,7 @@
   ###
 
   home-manager.users.l = flake-self.homeConfigurations.server;
+  home-manager.users.root = flake-self.homeConfigurations.server;
 
   link = {
     # make sure this module is compatible with ARM!
