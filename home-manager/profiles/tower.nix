07c5293a5930122cbfecf84b0506aaae159da691
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-11-14 20:31:55 +0100

    tower vscode zoom level

--- home-manager/profiles/tower.nix
+++ home-manager/profiles/tower.nix
@@ -1,5 +1,5 @@
 { lib, pkgs, flake-self, config, system-config, ... }:
 with lib; {
   imports = [ ./main.nix ];
-  config = { };
+  config = { programs.vscode.userSettings."window.zoomLevel" = -3; };
 }
