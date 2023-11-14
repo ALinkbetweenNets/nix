de98ff287cf4b12c1dd7dd55f9310959def9fc17
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-11-14 11:17:22 +0100

    onlyoffice

--- modules/onlyoffice/default.nix
+++ modules/onlyoffice/default.nix
@@ -1,15 +1,38 @@
 { config, system-config, pkgs, lib, ... }:
 with lib;
-let cfg = config.link.onlyoffice;
+let cfg = config.link.services.onlyoffice;
 in {
-  options.link.onlyoffice.enable = mkEnableOption "activate onlyoffice";
+  options.link.services.onlyoffice = {
+    enable = mkEnableOption "activate onlyoffice";
+    expose = mkOption {
+      type = types.bool;
+      default = config.link.expose;
+      description = "expose onlyoffice to the internet with NGINX and ACME";
+    };
+    nginx = mkOption {
+      type = types.bool;
+      default = config.link.nginx.enable;
+      description = "Use service with NGINX";
+    };
+  };
   config = mkIf cfg.enable {
     services = {
       onlyoffice = {
         enable = true;
-        hostname = "office.${config.link.domain}";
+        hostname = "onlyoffice.${config.link.domain}";
         port = 8000;
 
+      };
+      nginx.virtualHosts."onlyoffice.${config.link.domain}" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${toString config.services.onlyoffice.port}";
+        };
+        extraConfig = mkIf (!cfg.expose) ''
+          allow ${config.link.service-ip}/24;
+          deny all; # deny all remaining ips
+        '';
       };
     };
   };
