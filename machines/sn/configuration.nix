6d00c06d724e402bb09d8443332b365a834a247b
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-11-16 17:05:07 +0100

    root home-manager

--- machines/sn/configuration.nix
+++ machines/sn/configuration.nix
@@ -5,6 +5,7 @@
     home-manager.nixosModules.home-manager
   ];
   home-manager.users.l = flake-self.homeConfigurations.server;
+  home-manager.users.root = flake-self.homeConfigurations.server;
   link = {
     common.enable = true;
     grub.enable = true;
@@ -35,7 +36,7 @@
     # };
     # keycloak.enable = true;
     services.nextcloud = { enable = true; expose = true; };
-    services.hedgedoc = { enable = true; };
+    services.hedgedoc = { enable = true; expose = true; };
     outline.enable = true;
     paperless.enable = true;
     # photoprism.enable = true;
