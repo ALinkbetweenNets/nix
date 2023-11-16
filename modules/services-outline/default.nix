fb3d530661f0539c92f3d1e92f7c133c8e54a4fa
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-11-16 17:05:08 +0100

    outline proxypass scheme

--- modules/services-outline/default.nix
+++ modules/services-outline/default.nix
@@ -33,7 +33,7 @@ in {
         enableACME = true;
         forceSSL = true;
         locations."/" = {
-          proxyPass = "127.0.0.1:${toString config.services.outline.port}";
+          proxyPass = "http://127.0.0.1:${toString config.services.outline.port}";
         };
       };
     };
