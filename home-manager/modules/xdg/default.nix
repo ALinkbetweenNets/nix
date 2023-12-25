f196e5e73c345219bb42e2463fa3b6729c868506
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-12-25 16:32:37 +0100

    xdg mime audio -> vlc

--- home-manager/modules/xdg/default.nix
+++ home-manager/modules/xdg/default.nix
@@ -9,20 +9,23 @@ with lib;
     mimeApps.enable = true;
     mimeApps.defaultApplications = {
       "application/gzip" = [ "org.kde.ark.desktop" ];
-      "application/pdf" = [ "org.gnome.Evince.desktop" ];
       "application/vnd.rar" = [ "org.kde.ark.desktop" ];
       "application/x-7z-compressed" = [ "org.kde.ark.desktop" ];
       "application/x-bzip" = [ "org.kde.ark.desktop" ];
       "application/x-bzip2" = [ "org.kde.ark.desktop" ];
       "application/x-sh" = [ "codium.desktop" ];
       "application/x-tar" = [ "org.kde.ark.desktop" ];
-      "application/xml" = [ "org.kde.kate.desktop" ];
       "application/zip" = [ "org.kde.ark.desktop" ];
-      "audio/flac" = [ "mpv.desktop" ];
-      "audio/mpeg" = [ "mpv.desktop" ];
-      "audio/ogg" = [ "mpv.desktop" ];
-      "audio/opus" = [ "mpv.desktop" ];
-      "audio/webm" = [ "mpv.desktop" ];
+
+      "application/pdf" = [ "org.gnome.Evince.desktop" ];
+      "application/xml" = [ "org.kde.kate.desktop" ];
+
+      "audio/flac" = [ "vlc.desktop" ];
+      "audio/mpeg" = [ "vlc.desktop" ];
+      "audio/ogg" = [ "vlc.desktop" ];
+      "audio/opus" = [ "vlc.desktop" ];
+      "audio/webm" = [ "vlc.desktop" ];
+
       "image/bmp" = [ "org.kde.gwenview.desktop" ];
       "image/gif" = [ "org.kde.gwenview.desktop" ];
       "image/jpeg" = [ "org.kde.gwenview.desktop" ];
@@ -30,15 +33,19 @@ with lib;
       "image/png" = [ "org.kde.gwenview.desktop" ];
       "image/svg+xml" = [ "org.kde.gwenview.desktop" ];
       "image/webp" = [ "org.kde.gwenview.desktop" ];
+
       "text/calendar" = [ "thunderbird.desktop" ];
       "text/javascript" = [ "codium.desktop" ];
       "text/plain" = [ "org.kde.kate.desktop" ];
       "text/xml" = [ "org.kde.kate.desktop" ];
+
       "video/mp4" = [ "mpv.desktop" ];
       "video/mpeg" = [ "mpv.desktop" ];
       "video/webm" = [ "mpv.desktop" ];
+
       "x-scheme-handler/sgnl" = [ "signal-desktop.desktop" ];
       "x-scheme-handler/signalcaptcha" = [ "signal-desktop.desktop" ];
+
       "x-scheme-handler/http" = [ "firefox.desktop" ];
       "x-scheme-handler/https" = [ "firefox.desktop" ];
       "text/html" = [ "firefox.desktop" ];
