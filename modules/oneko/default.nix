52bda67c7add2466eb81e36df7a1d4021db7c2a0
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-12-25 14:42:46 +0100

    oneko systemd-service fix

--- modules/oneko/default.nix
+++ modules/oneko/default.nix
@@ -4,17 +4,19 @@ let
   cfg = config.link.oneko;
 in
 {
-  options.link.oneko.enable = mkEnableOption "activate oneko";
+  options.link.oneko.enable = mkEnableOption "release oneko";
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [ oneko ];
     systemd.user.services.oneko = {
+      enable = true;
       description = "oneko";
       serviceConfig.PassEnvironment = "DISPLAY";
       script = ''
         oneko -sakura
       '';
       path = with pkgs; [ oneko ];
-      wantedBy = [ "multi-user.target" ]; # starts after login, reboot after first time rebuild
+      wantedBy = [ "graphical-session.target" ]; # starts after login, reboot after first time rebuild
+      after = [ "network.target" ];
     };
   };
 }
