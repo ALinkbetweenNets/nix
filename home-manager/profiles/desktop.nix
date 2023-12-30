216b595c14816c225518a511f3045f23f83e39cd
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-12-30 14:51:41 +0100

    mediainfo, tradingview, yewtube, restructuring

--- home-manager/profiles/desktop.nix
+++ home-manager/profiles/desktop.nix
@@ -2,60 +2,6 @@
 with lib; {
   imports = [ ./common.nix flake-self.homeManagerModules.xdg ];
   config = {
-    # Packages to install on all desktop systems
-    home.packages = with pkgs;
-      [
-        sshfs
-        nfs-utils
-        ## Basics
-        hunspell
-        hunspellDicts.de_DE
-        # Audio
-        helvum # Patchbay
-        pavucontrol
-        # Multimedia
-        vlc
-        mpv
-        # Encryption
-        veracrypt
-        kleopatra # gpg/ pgp
-        partition-manager
-        # Misc
-        xdg-utils
-        # Editor
-        libsForQt5.kate # kate
-        kdiff3
-        obsidian
-        # Hex Editor
-        hexdino # terminal vimlike hex editor
-        okteta # hex editor
-        # File Sync
-        #syncthing-tray
-        # Multimedia
-        cobang # qr codes
-        # Privacy
-        # tor-browser-bundle-bin # compromised
-        #mullvad-vpn
-        mullvad-browser
-        ktailctl
-        # RDP
-        remmina # VNC Client
-        libsForQt5.krfb # kde connect virtual monitor
-        libsForQt5.kpurpose # KDE share
-        scrcpy # ADB screenshare
-        libsForQt5.qt5.qtwebsockets
-        libsForQt5.calendarsupport
-        libsForQt5.konsole
-        libsForQt5.yakuake
-        libsForQt5.plasma-vault
-        libsForQt5.plasma-disks
-        libsForQt5.kfind
-        ktailctl # Tailscale GUI
-        # vivaldi
-        bitwarden
-        (pkgs.nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
-      ] ++ lib.optionals
-        (system-config.nixpkgs.hostPlatform.system == "x86_64-linux") [ ];
     programs = {
       firefox.enable = true;
       yt-dlp = {
@@ -67,6 +13,60 @@ with lib; {
     };
     manual.html.enable = true;
     link = { code.enable = true; };
-
+    # Packages to install on all desktop systems
+    home.packages = with pkgs;
+      [
+        ## Basics
+        sshfs
+        nfs-utils
+        ## Spelling
+        hunspell
+        hunspellDicts.de_DE
+        ## Audio
+        helvum # Patchbay
+        pavucontrol
+        ## Multimedia
+        vlc
+        mpv
+        cobang # qr codes
+        ## Encryption
+        veracrypt
+        kleopatra # gpg/ pgp
+        partition-manager
+        ## Misc
+        bitwarden
+        xdg-utils
+        scrcpy # ADB screenshare
+        ktailctl # Tailscale GUI
+        ## Editor
+        libsForQt5.kate # kate
+        kdiff3
+        obsidian
+        ## Hex Editor
+        hexdino # terminal vimlike hex editor
+        okteta # hex editor
+        ## File Sync
+        #syncthing-tray
+        ## Browser
+        # tor-browser-bundle-bin # compromised
+        # vivaldi
+        floorp
+        #mullvad-vpn
+        mullvad-browser
+        ## RDP
+        remmina # VNC Client
+        ## KDE Utils
+        libsForQt5.krfb # kde connect virtual monitor
+        libsForQt5.kpurpose # KDE share
+        libsForQt5.qt5.qtwebsockets
+        libsForQt5.calendarsupport
+        libsForQt5.konsole
+        libsForQt5.yakuake
+        libsForQt5.plasma-vault
+        libsForQt5.plasma-disks
+        libsForQt5.kfind
+        (pkgs.nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+      ] ++ lib.optionals
+        (system-config.nixpkgs.hostPlatform.system == "x86_64-linux") [ ];
   };
 }
