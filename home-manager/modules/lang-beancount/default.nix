f1f00745b7ab8f0d5ef3ed330a31636eac97ab99
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2023-12-30 14:44:34 +0100

    beancount module

---
+++ home-manager/modules/lang-beancount/default.nix
@@ -0,0 +1,26 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.link.beancount;
+in
+{
+  options.link.beancount.enable = mkEnableOption "activate beancount toolchain";
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      beancount # bookkeeping
+    ];
+    programs.vscode.extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "beancount";
+        publisher = "lencerf";
+        version = "0.10.0";
+        sha256 = "sha256-xsGYr9Aqfoe16U9lACyGkTfknwMf0n2oOog498SS26Y=";
+      }
+      {
+        name = "vscode-beancount-formatter";
+        publisher = "dongfg";
+        version = "1.4.2";
+        sha256 = "sha256-LLytNvXxcoDvTKiTuQWVPV8niPM8Bvi3R+zO1lxUM0U=";
+      }
+    ];
+  };
+}
