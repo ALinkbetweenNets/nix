377a4f46325a0dedb389bc87176cd1b5c9fd923f
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-01-09 17:29:25 +0100

    sn restic onedrive

--- modules/restic/restic.nix
+++ modules/restic/restic.nix
@@ -17,6 +17,12 @@ in
       example = [ "/var/lib/gitea" ];
       description = "Paths to backup to sn";
     };
+    backup-paths-onedrive = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+      example = [ "/var/lib/gitea" ];
+      description = "Paths to backup to onedrive";
+    };
     backup-paths-rsn = mkOption {
       type = types.listOf types.str;
       default = [ ];
@@ -99,6 +105,30 @@ in
           repositoryFile = config.sops.secrets."restic/sn/repository".path;
           passwordFile = config.sops.secrets."restic/sn/password".path;
           environmentFile = config.sops.secrets."restic/sn/environment".path;
+          pruneOpts = [
+            "--keep-daily 7"
+            "--keep-weekly 5"
+            "--keep-monthly 12"
+            "--keep-yearly 75"
+          ];
+          timerConfig = {
+            OnCalendar = "03:00";
+            Persistent = true;
+            RandomizedDelaySec = "5h";
+          };
+          extraBackupArgs = [
+            "--exclude-file=${restic-ignore-file}"
+            "--one-file-system"
+            "--compression=max"
+            # "--dry-run"
+            "-v"
+          ];
+          initialize = true;
+        };
+        onedrive = {
+          paths = cfg.backup-paths-onedrive;
+          repositoryFile = config.sops.secrets."restic/onedrive/repository".path;
+          passwordFile = config.sops.secrets."restic/onedrive/password".path;
           pruneOpts = [
             "--keep-daily 7"
             "--keep-weekly 5"
