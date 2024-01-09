7d57155f9f31710afc61f214974a88b58d1fa806
ALinkbetweenNets <--global>
2024-01-09 20:33:03 +0100

    more nginx transition from sn to nc

--- modules/containers/diagrams.nix
+++ modules/containers/diagrams.nix
@@ -16,17 +16,6 @@ in
       volumes = [ "${config.link.storage}/diagrams:/persist" ];
       ports = [ "8765:8080" ];
     };
-    services.nginx.virtualHosts."diagrams.${config.link.domain}" = {
-      enableACME = true;
-      forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:8765";
-      };
-      # extraConfig = mkIf (!cfg.expose) ''
-      #   allow ${config.link.service-ip}/24;
-      #     allow 127.0.0.1;
-      #     deny all; # deny all remaining ips
-      # '';
-    };
+    networking.firewall.interfaces."${config.link.service-interface}".allowedTCPPorts = [ 8765 ];
   };
 }
