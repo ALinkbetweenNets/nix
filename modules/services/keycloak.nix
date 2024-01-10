432bb0ac0f6daf1cbee74f743f48352c740266c3
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-01-10 20:39:57 +0100

    keycloak modularization

--- modules/services/keycloak.nix
+++ modules/services/keycloak.nix
@@ -1,8 +1,30 @@
 { config, system-config, pkgs, lib, ... }:
 with lib;
-let cfg = config.link.keycloak;
+let cfg = config.link.services.keycloak;
 in {
-  options.link.keycloak.enable = mkEnableOption "activate keycloak";
+    options.link.services.keycloak = {
+    enable = mkEnableOption "activate keycloak";
+    expose-port = mkOption {
+      type = types.bool;
+      default = config.link.service-ports-expose;
+      description = "directly expose the port of the application";
+    };
+    nginx = mkOption {
+      type = types.bool;
+      default = config.link.nginx.enable;
+      description = "expose the application to the internet with NGINX and ACME";
+    };
+    nginx-expose = mkOption {
+      type = types.bool;
+      default = config.link.nginx-expose;
+      description = "expose the application to the internet";
+    };
+      port = mkOption {
+      type = types.int;
+      default = 31123;
+      description = "port to run the application on";
+    };
+  };
   config = mkIf cfg.enable {
     environment.noXlibs = false;
     services = {
@@ -17,8 +39,8 @@ in {
         settings = {
           hostname = "keycloak.${config.link.domain}";
           hostname-strict-backchannel = true;
-          http-host = "127.0.0.1";
-          http-port = 31123;
+          http-host = if cfg.expose-port then "0.0.0.0" else "127.0.0.1";
+          http-port = cfg.port;
           http-relative-path = "/";
           proxy = "edge";
         };
@@ -29,7 +51,7 @@ in {
           forceSSL = true;
           locations = {
             "/" = {
-              proxyPass = "http://localhost:${toString config.services.keycloak.settings.http-port}/";
+              proxyPass = "http://localhost:${toString cfg.port}";
               extraConfig = ''
                 proxy_set_header X-Forwarded-Host $http_host;
                 proxy_set_header X-Real-IP $remote_addr;
