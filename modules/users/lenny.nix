af2f723c82e0ef1a16eec93fdf6a27847516d047
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-01-12 13:05:50 +0100

    lenny debug key

--- modules/users/lenny.nix
+++ modules/users/lenny.nix
@@ -11,10 +11,14 @@ in {
       #   ++ lib.optionals config.networking.networkmanager.enable
       #   [ "networkmanager" ];
       # shell = "${pkgs.zsh}/bin/zsh";
-      openssh.authorizedKeys.keys = [
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDLoxnTT9WVrfIqks5UeHQNstAUZlU+1fK/s4EoKLFYGifrtZJv3E3O8VuwxZyndjLRfwYYorKnyVqmdm9ebMnMgbzZPBfwkVdMMOEhO8JDgCC38A+cGZ3bJie1Wi7BiJnNhKAgcGdv+tsdt8AQfwJQRXdZV7NvNeBVs0XqZc05feP8jRAzh2HEAk0csU6U51doIGmbh+69iAHQhBqjy1LcF6VQmnf9vjVo2yiF/vl+xsDO5tWcbdZwnH7WL6qKCOT3xGqTJN6CA89gK/jvPuAdpi//VO0yVHypqXXqZCbYIs6D9TOdLOHPaBxSENwD/5MOJILzaX06LTLxrWV0FOhrRNh42QnFWyoIh/To1135rpz1PJDb8NGHqelKkRBF3HDSqRl0N9kdhFvjrEFph9hMYjb6DyAgkRSKHGDJfAwaeJwdNj5yv0iXX1ZfZcRPMgWdL5MZz2TWEBcBxi20/jxx4yooTNdvE09IlxQWr81flyhpSnBwd/ph1YyTkYKH548= lb@peridot"
+      openssh.authorizedKeys.keyFiles = [
+        config.sops.secrets."users/lenny/pubkey".path
+        config.sops.secrets."users/lenny/debugkey".path
       ];
     };
+    sops.secrets."users/lenny/pubkey" = { neededForUsers = true; };
+    sops.secrets."users/lenny/debugkey" = { neededForUsers = true; };
+    # Note on sftp chrooting: The chroot folder must be owned by root and set to 755. Inside that folder create one or more folders owned by the user
     services.openssh.extraConfig = ''
       Match User lenny
           ChrootDirectory /rz/sftp/lenny
