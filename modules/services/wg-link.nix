77287cc44c636551c2a7a4d3b2598c7fc3dbc309
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-01-16 11:32:41 +0100

    disable 10.0.1.x

--- modules/services/wg-link.nix
+++ modules/services/wg-link.nix
@@ -5,23 +5,6 @@ in {
   options.link.services.wg-link.enable = mkEnableOption "activate wg-link";
   config = mkIf cfg.enable {
     networking.nat.internalInterfaces = [ "wg-link" ];
-    networking.extraHosts =
-      ''
-        10.0.1.1 linkserver.org
-        10.0.1.1 jitsi.linkserver.org
-        10.0.1.1 jellyfin.linkserver.org
-        10.0.1.1 jellyseer.linkserver.org
-        10.0.1.1 gitea.linkserver.org
-        10.0.1.1 paperless.linkserver.org
-        10.0.1.1 hedgedoc.linkserver.org
-        10.0.1.1 alinkbetweennets
-        10.0.1.1 nextcloud.linkserver.org
-        10.0.1.1 matrix.linkserver.org
-        10.0.1.1 onlyoffice.linkserver.org
-        10.0.1.1 vaultwarden.linkserver.org
-        10.0.1.1 element.linkserver.org
-        10.0.1.1 outline.linkserver.org
-      '';
     networking.wireguard.interfaces = {
       wg-link = {
         # Determines the IP/IPv6 address and subnet of the client's end of the tunnel interface
