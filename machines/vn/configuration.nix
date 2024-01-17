bbe772a8ef1ef403a121c346aa574e090e331b66
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-01-17 15:28:15 +0100

    vn machine

---
+++ machines/vn/configuration.nix
@@ -0,0 +1,30 @@
+{ self, ... }:
+{ pkgs, lib, config, flake-self, home-manager, ... }: {
+  imports = [
+    ./hardware-configuration.nix
+    home-manager.nixosModules.home-manager
+  ];
+  home-manager.users.l = flake-self.homeConfigurations.server;
+  link = {
+    systemd-boot.enable = false;
+    grub.enable = true;
+    vm.enable = true;
+    tailscale.enable = true;
+    tailscale.routing = "server";
+  };
+  networking.hostName = "vn";
+  # boot.loader.grub.device = "/dev/vda";
+  # services.mullvad-vpn.enable = true;
+  # services.mullvad-vpn.package = pkgs.mullvad-vpn; # gui version
+  # fileSystems."arra" = {
+  #   device = "arra"; # Replace with the correct device or path
+  #   fsType = "9p"; # Replace with the filesystem type
+  #   mountPoint = "${config.link.storage}";
+  #   options = [ "trans=virtio" ];
+  # };
+  # services.tailscale = {
+  #   enable = true;
+  #   useRoutingFeatures = "server";
+  #   extraUpFlags = [ "--advertise-exit-node" ];
+  # };
+}
