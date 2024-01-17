22f62cbe6cd253eaebd96ea91aa6a8a817e5e313
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-01-17 15:16:35 +0100

    remove iptables remnants

--- modules/services/wg-link.nix
+++ modules/services/wg-link.nix
@@ -15,13 +15,13 @@ in {
         privateKeyFile = "/home/l/.keys/wg-link.private";
         # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
         # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
-        postSetup = ''
-          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
-        '';
-        # This undoes the above command
-        postShutdown = ''
-          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
-        '';
+        # postSetup = ''
+        #   ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+        # '';
+        # # This undoes the above command
+        # postShutdown = ''
+        #   ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+        # '';
         peers = [
           {
             # l
