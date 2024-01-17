7034be83e49df8836ba20d758e73e0d46df19bfc
ALinkbetweenNets <alinkbetweennets@protonmail.com>
2024-01-17 15:28:04 +0100

    fix and reenable zsh

--- modules/users/l.nix
+++ modules/users/l.nix
@@ -17,7 +17,7 @@ in {
         ++ lib.optionals config.link.docker.enable
         [ "docker" ]
       ;
-      shell = "${pkgs.fish}/bin/fish";
+      shell = "${pkgs.zsh}/bin/zsh";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/1eePE6/pYo4aahzcDRqbmnVdx9ikKH+93yw7M1pXH l@xn"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINI74luZ3xJcgaZYHzn5DtSpYufml+SbhZQV12gWGShS l@dn"
