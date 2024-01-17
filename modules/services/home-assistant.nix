a23c0c796fbb5baffe401bba9694ad1d0dee5e1f
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-01-17 18:17:04 +0100

    home assistant podman->docker

--- modules/services/home-assistant.nix
+++ modules/services/home-assistant.nix
@@ -10,8 +10,8 @@ in {
         environment.TZ = "Europe/Berlin";
         image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
         extraOptions = [
-          # "--network=host"
-          "--cap-add=CAP_NET_RAW,CAP_NET_BIND_SERVICE"
+          "--network=host"
+          # "--cap-add=CAP_NET_RAW,CAP_NET_BIND_SERVICE"
           # "--device=/dev/ttyACM0:/dev/ttyACM0"  # Example, change this to match your own hardware
         ];
       };
