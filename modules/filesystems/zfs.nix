f814ea30823f1340b20862f56d699b82af17b633
ALinkbetweenNets <alinkbetweennets@protonmail.com>
2024-01-22 14:12:21 +0100

    zfs options

--- modules/filesystems/zfs.nix
+++ modules/filesystems/zfs.nix
@@ -4,13 +4,31 @@ let cfg = config.link.fs.zfs;
 in {
   options.link.fs.zfs.enable = mkEnableOption "activate zfs";
   config = mkIf cfg.enable {
-    boot.loader.grub.zfsSupport = true;
-    services.zfs.autoScrub.enable = true;
-    services.zfs.trim.enable = true;
-    boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
-    boot.supportedFilesystems = [ "zfs" ];
-    #virtualisation.docker.storageDriver = "zfs";
-    boot.zfs.forceImportRoot = true;
-    boot.zfs.forceImportAll = true;
+    services.zfs = {
+      autoScrub.enable = true;
+      trim.enable = true;
+      # zed.settings = {
+      #   ZED_DEBUG_LOG = "/tmp/zed.debug.log";
+
+      #   ZED_EMAIL_ADDR = [ "root" ];
+      #   ZED_EMAIL_PROG = "mail";
+      #   ZED_EMAIL_OPTS = "-s '@SUBJECT@' @ADDRESS@";
+
+      #   ZED_NOTIFY_INTERVAL_SECS = 3600;
+      #   ZED_NOTIFY_VERBOSE = false;
+      #   ZED_SCRUB_AFTER_RESILVER = false;
+      # };
+    };
+    boot = {
+      loader.grub.zfsSupport = true;
+      kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+      supportedFilesystems = [ "zfs" ];
+      zfs = {
+        #virtualisation.docker.storageDriver = "zfs";
+        forceImportRoot = true;
+        forceImportAll = true;
+        # package.enableMail = true;
+      };
+    };
   };
 }
