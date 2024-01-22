eb0b7116fa2f51a3b2e1c63c5fbcf787ce7e2769
ALinkbetweenNets <alinkbetweennets@protonmail.com>
2024-01-22 13:17:51 +0100

    sn disable ipv4 forward

--- machines/sn/configuration.nix
+++ machines/sn/configuration.nix
@@ -115,7 +115,7 @@
     eth = "enp6s0";
   };
 
-  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+  # boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
 
   networking.firewall.interfaces."${config.link.service-interface}".allowedTCPPorts = [ 31337 ];
   networking.firewall.allowedUDPPorts = [ 51821 ];
