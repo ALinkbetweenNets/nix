1f52794bbefc3a8ec31f83c7c91be5a43a9e62d6
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-01-26 17:36:36 +0100

    vn

--- machines/vn/hardware-configuration.nix
+++ machines/vn/hardware-configuration.nix
@@ -6,18 +6,19 @@
   imports =
     [
       (modulesPath + "/profiles/qemu-guest.nix")
+      ./disk-config.nix
     ];
   boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  # boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
-  fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/96961cea-6008-4232-854f-51b5895130fe";
-      fsType = "ext4";
-    };
-  swapDevices =
-    [{ device = "/dev/disk/by-uuid/69958a46-8fa9-4a5b-9b00-eab8189edd8b"; }];
+  # fileSystems."/" =
+  #   {
+  #     device = "/dev/disk/by-uuid/96961cea-6008-4232-854f-51b5895130fe";
+  #     fsType = "ext4";
+  #   };
+  # swapDevices =
+  #   [{ device = "/dev/disk/by-uuid/69958a46-8fa9-4a5b-9b00-eab8189edd8b"; }];
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
