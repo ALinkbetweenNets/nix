a782f4b028f628e202c587f7fbfdc7fb23e06aa2
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-01-27 14:46:02 +0100

    moved xdg fully into home manager

--- modules/ds-wayland/default.nix
+++ modules/ds-wayland/default.nix
@@ -22,19 +22,6 @@ in {
       MOZ_ENABLE_WAYLAND = "1";
     };
     services.dbus.enable = true;
-    xdg = {
-      mime.enable = true;
-      icons.enable = true;
-      portal = {
-        enable = true;
-        wlr.enable = true;
-        extraPortals = with pkgs; [
-          # xdg-desktop-portal-wlr
-          # xdg-desktop-portal-gtk
-        ];
-      };
-    };
-
     programs.light.enable = true;
 
     security = {
