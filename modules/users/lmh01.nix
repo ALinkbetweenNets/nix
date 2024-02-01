b5b90caf33317f0a2e8f551f01a47a33a63ffe5d
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-02-01 17:40:03 +0100

    lmh pubkey

--- modules/users/lmh01.nix
+++ modules/users/lmh01.nix
@@ -11,6 +11,10 @@ in {
       #   ++ lib.optionals config.networking.networkmanager.enable
       #   [ "networkmanager" ];
       # shell = "${pkgs.zsh}/bin/zsh";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMHnB5ZsoYbWAa5ndFOngbcLz14yHwewX9Fp62cBkcpw"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZC4KwR8QrKvOgClilSOhWFh7rblPoRbvVRZ6+uVhNn"
+      ];
     };
     # Note on sftp chrooting: The chroot folder must be owned by root and set to 755. Inside that folder create one or more folders owned by the user
     services.openssh.extraConfig = ''
