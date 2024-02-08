02c0942baaf3d1787a04b7823ecccbbb90e15908
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-02-08 20:27:03 +0100

    in plasma->gnome

--- machines/in/configuration.nix
+++ machines/in/configuration.nix
@@ -8,11 +8,14 @@
   link = {
     systemd-boot.enable = true;
     convertible.enable = true;
-    main.enable = true;
+    # main.enable = true;
     cpu-intel.enable = true;
+    plasma.enable = false;
+    gnome.enable = true;
   };
   networking.hostName = "in";
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  lollypops.deployment = { local-evaluation = true; ssh.host = "192.168.178.65"; };
   environment.systemPackages = with pkgs;    [ ];
   #system.stateVersion = "23.05";
 }
