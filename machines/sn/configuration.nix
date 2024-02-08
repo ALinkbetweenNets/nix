8387468c2c3851c67fb54d636a5b96be51687914
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-02-08 17:26:33 +0100

    sn reformatting, xandikos & nextcloud disabled

--- machines/sn/configuration.nix
+++ machines/sn/configuration.nix
@@ -29,54 +29,26 @@
     service-interface = "tailscale0";
     users.lenny.enable = true;
     users.lmh01.enable = true;
-
     nginx.enable = false;
     containers = {
-      grist = {
-        enable = true;
-        expose-port = true;
-      };
-      diagrams = {
-        enable = true;
-        expose-port = true;
-      };
+      grist.enable = true;
+      diagrams.enable = true;
     };
     # zola.enable = true;
     syncthing.enable = true;
+    nginx-expose = false;
+    service-ports-expose = true;
     services = {
-      # photoprism = {
-      #   enable = true;
-      #   expose-port = true;
-      # };
-      # keycloak.enable = true;
-      gitea = {
-        enable = true;
-        expose-port = true;
-      };
-      grafana = {
-        enable = true;
-        expose-port = true;
-      };
-      # seafile = {
-      #   enable = true;
-      #   expose-port = true;
-      # };
-      hedgedoc = {
-        enable = true;
-        expose-port = true;
-      };
-      jellyfin = {
-        enable = true;
-        expose-port = true;
-      };
-      jellyseer = {
-        enable = true;
-        expose-port = true;
-      };
-      minio = {
-        enable = true;
-        expose-port = true;
-      };
+      # photoprism.enable = true; # WIP
+      # keycloak.enable = true; # WIP but running
+      gitea.enable = true;
+      grafana.enable = true;
+      seafile.enable = true;
+      # xandikos.enable = true; # WIP
+      hedgedoc.enable = true;
+      jellyfin.enable = true;
+      jellyseer.enable = true;
+      minio.enable = true;
       # nextcloud = {
       #   enable = true;
       #   nginx-expose = true;
@@ -86,30 +58,26 @@
         nginx-expose = true;
         oidClientId = "2085b101-ee5c-42c1-acac-2f9265767d1f";
       };
-      paperless = {
-        enable = true;
-        expose-port = true;
-      };
-      restic-client.enable = true;
-      restic-client.backup-paths-onedrive = [
-        "/home/l/.ssh"
-        "/rz/syncthing"
-      ];
-      restic-client.backup-paths-lenny-storagebox = [
-        "/home/l/.ssh"
-        "/rz/syncthing/uni"
-        "/rz/syncthing/sec"
-        "/rz/syncthing/music"
-        "/rz/syncthing/doc"
-      ];
-      vaultwarden = {
-        enable = true;
-        nginx-expose = false;
-      };
+      paperless.enable = true;
+      vaultwarden.enable = true;
       # matrix.enable = true;
       # restic-server = { enable = true; expose = false; };
       coturn.enable = true;
       # dns.enable = true;
+      restic-client = {
+        enable = true;
+        backup-paths-onedrive = [
+          "/home/l/.ssh"
+          "/rz/syncthing"
+        ];
+        backup-paths-lenny-storagebox = [
+          "/home/l/.ssh"
+          "/rz/syncthing/uni"
+          "/rz/syncthing/sec"
+          "/rz/syncthing/music"
+          "/rz/syncthing/doc"
+        ];
+      };
       wg-link.enable = true;
       # services.jitsi = {
       #   enable = true;
@@ -117,17 +85,12 @@
       # };
       # keycloak.enable = true;
     };
-    nginx-expose = false;
-    service-ports-expose = true;
     eth = "enp6s0";
   };
-
   # boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
-
   networking.firewall.interfaces."${config.link.service-interface}".allowedTCPPorts = [ 31337 ];
   networking.firewall.allowedUDPPorts = [ 51821 ];
   networking.firewall.allowedTCPPorts = [ 51821 ];
-
   fileSystems."/rz/sftp/lenny/arr" = {
     device = "/rz/arr/lenny/";
     options = [ "bind" ];
@@ -156,8 +119,6 @@
   # systemd.services.docker-librespeedtest = {
   #   preStop = "${pkgs.docker}/bin/docker kill librespeedtest";
   # };
-
-
   powerManagement.powertop.enable = true;
   # virtualisation.sharedDirectories = {
   #   arr = {
