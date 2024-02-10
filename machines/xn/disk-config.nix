898749692a6ed38d5afb8ea59cb77b4db8223c52
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-02-10 11:35:40 +0100

    xn restic remove doc

--- machines/xn/disk-config.nix
+++ machines/xn/disk-config.nix
@@ -25,12 +25,12 @@
                 type = "luks";
                 name = "crypted";
                 # disable settings.keyFile if you want to use interactive password entry
-                passwordFile = "/tmp/luks.key"; # Interactive
+                # passwordFile = "/tmp/luks.key"; # Interactive
                 settings = {
                   allowDiscards = true;
                   #keyFile = "/tmp/secret.key";
                 };
-                additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+                # additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
                 content = {
                   type = "btrfs";
                   extraArgs = [ "-f" ];
