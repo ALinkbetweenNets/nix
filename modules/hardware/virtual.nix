241d080202044018b523d88a936c75288768691a
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-05-06 10:22:01 +0200

    proxmox changes for virtual hardware

--- modules/hardware/virtual.nix
+++ modules/hardware/virtual.nix
@@ -8,5 +8,16 @@ in {
     environment.systemPackages = with pkgs; [
       virtiofsd
     ];
+    services.qemuGuest.enable = true;
+    fileSystems."/".autoResize = true;
+    boot.growPartition = true;
+    services.fstrim = {
+      enable = true;
+      interval = "weekly";
+    };
+    boot.initrd = {
+      availableKernelModules = [ "virtio_net" "virtio_pci" "virtio_mmio" "virtio_blk" "virtio_scsi" "9p" "9pnet_virtio" ];
+      kernelModules = [ "virtio_balloon" "virtio_console" "virtio_rng" ];
+    };
   };
 }
