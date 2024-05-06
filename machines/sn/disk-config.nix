81272692d9735f8764f2db323e7c66a3b5bb7b65
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-05-06 10:22:14 +0200

    sn disk formatting

--- machines/sn/disk-config.nix
+++ machines/sn/disk-config.nix
@@ -1,66 +1,75 @@
 {
   disko.devices = {
     disk = {
-      nvme0n1 = {
+      main = {
         type = "disk";
         device = "/dev/sda";
         content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              priority = 1;
-              size = "1M";
-              type = "EF02";
-            };
-            ESP = {
-              size = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [
-                  "defaults"
-                ];
-              };
-            };
-            luks = {
-              size = "100%";
-              content = {
-                type = "luks";
-                name = "crypted";
-                # disable settings.keyFile if you want to use interactive password entry
-                # passwordFile = "/tmp/luks.key"; # Interactive
-                settings = {
-                  allowDiscards = true;
-                  #keyFile = "/tmp/secret.key";
-                };
-                # additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+          type = "table";
+          format = "gpt";
+          partitions =
+            [
+              {
+                name = "boot";
+                start = "0";
+                end = "1M";
+                # type = "EF02";
+                flags = [ "bios_grub" ];
+              }
+              {
+                name = "ESP";
+                start = "1M";
+                end = "512M";
+                # type = "EF00";
+                bootable = true;
                 content = {
-                  type = "btrfs";
-                  extraArgs = [ "-f" ];
-                  subvolumes = {
-                    "/root" = {
-                      mountpoint = "/";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/home" = {
-                      mountpoint = "/home";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/swap" = {
-                      mountpoint = "/.swapvol";
-                      swap.swapfile.size = "64G";
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  #mountOptions = [
+                  #  "defaults"
+                  #];
+                };
+              }
+              {
+                name = "luks";
+                start = "512M";
+                end = "100%";
+                content = {
+                  type = "luks";
+                  name = "crypted";
+                  # disable settings.keyFile if you want to use interactive password entry
+                  # passwordFile = "/tmp/luks.key"; # Interactive
+                  settings = {
+                    allowDiscards = true;
+                    #keyFile = "/tmp/secret.key";
+                  };
+                  # additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+                  content = {
+                    type = "btrfs";
+                    extraArgs = [ "-f" ];
+                    subvolumes = {
+                      "/root" = {
+                        mountpoint = "/";
+                        mountOptions = [ "compress=zstd" "noatime" ];
+                      };
+                      "/home" = {
+                        mountpoint = "/home";
+                        mountOptions = [ "compress=zstd" "noatime" ];
+                      };
+                      "/nix" = {
+                        mountpoint = "/nix";
+                        mountOptions = [ "compress=zstd" "noatime" ];
+                      };
+                      "/swap" = {
+                        mountpoint = "/.swapvol";
+                        swap.swapfile.size = "32G";
+                      };
                     };
                   };
                 };
-              };
-            };
-          };
+              }
+            ];
         };
       };
     };
