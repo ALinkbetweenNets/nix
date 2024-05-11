1a99878025e12722b7947985fd9b9dbc0628b8c2
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-05-11 18:52:20 +0200

    added gitlab-ssh

--- machines/nc/configuration.nix
+++ machines/nc/configuration.nix
@@ -58,217 +58,235 @@
       }];
     };
   };
+  services.nginx.virtualHosts = {
 
-  services.nginx.virtualHosts."${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    default = true;
-    locations."/" = {
-      return = "301 https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    "${config.link.domain}" = {
+      enableACME = true;
+      forceSSL = true;
+      default = true;
+      locations."/" = {
+        return = "301 https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+      };
     };
-  };
-  # services.nginx.virtualHosts."grist.${config.link.domain}" = {
-  #   enableACME = true;
-  #   forceSSL = true;
-  #   locations."/" = {
-  #     proxyPass = "http://127.0.0.1:8484";
-  #     proxyWebsockets = true;
+    # services.nginx.virtualHosts."grist.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   locations."/" = {
+    #     proxyPass = "http://127.0.0.1:8484";
+    #     proxyWebsockets = true;
+    #   };
+    # services.nginx.virtualHosts."diagrams.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   locations."/" = {
+    #     proxyPass = "http://127.0.0.1:8765";
+    #     proxyWebsockets = true;
+    #   };
+    #   # extraConfig = mkIf (!cfg.expose) ''
+    #   #   allow ${config.link.service-ip}/24;
+    #   #     allow 127.0.0.1;
+    #   #     deny all; # deny all remaining ips
+    #   # '';
+    # };
+    # "matrix.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   locations."/" = {
+    #     proxyPass = "http://${config.link.serviceHost}:${toString config.link.services.matrix.port}";
+    #   };
+    # };
+    # "gitea.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   locations."/" = {
+    #     proxyPass = "http://${config.link.serviceHost}:${toString config.services.gitea.settings.server.HTTP_PORT}";
+    #   };
+    # };
+    "gitlab.${config.link.domain}" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://10.10.10.89:32961/}";
+      };
+    };
+    "gitlabssh.${config.link.domain}" = {
+      enableACME = true;
+      forceSSL = true;
+      listen = [{ port = 222; addr = "0.0.0.0"; ssl=true; }];
+      locations."/" = {
+        proxyPass = "10.10.10.89:32962";
+        # extraConfig = "proxy_http_version 1.0;" + "client_max_body_size 222M;";
+      };
+    };
+    # "grafana.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   locations."/" = {
+    #     proxyPass = "http://${config.link.serviceHost
+    #         }:${toString config.link.services.grafana.port}/";
+    #     proxyWebsockets = true;
+    #   };
+    # };
+    # "hedgedoc.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+
+    #   locations."/".proxyPass = "http://${config.link.serviceHost}:${toString config.link.services.hedgedoc.port}";
+
+    # };
+    # "jellyfin.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   listen = [{ port = 443; addr = "0.0.0.0"; ssl = true; } { port = 8096; addr = "0.0.0.0"; ssl = true; } { port = 8920; addr = "0.0.0.0"; ssl = true; }];
+    #   locations."/".proxyPass = "http://${config.link.serviceHost}:8096/";
+    # };
+    # "jellyfin1.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   locations."/".proxyPass = "http://${config.link.serviceHost}:8096/";
+    # };
+    # "jellyfin2.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   locations."/".proxyPass = "http://${config.link.serviceHost}:8920/";
+    # };
+    # "jellyseer.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   locations."/".proxyPass = "http://${config.link.serviceHost}:5055/";
+    # };
+    # "minio.s3.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   locations."/" = {
+    #     proxyPass = "http://${config.link.serviceHost}:9001";
+    #     proxyWebsockets = true;
+    #     extraConfig = ''
+    #       proxy_set_header X-Real-IP $remote_addr;
+    #       proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    #       proxy_set_header X-Forwarded-Proto $scheme;
+    #       # proxy_set_header Host $host;
+    #       proxy_connect_timeout 300;
+    #       # Default is HTTP/1, keepalive is only enabled in HTTP/1.1
+    #       #proxy_http_version 1.1;
+    #       proxy_set_header Connection "";
+    #       chunked_transfer_encoding off;
+    #     '';
+    #   };
+    #   extraConfig = ''
+    #     # To allow special characters in headers
+    #     ignore_invalid_headers off;
+    #     # Allow any size file to be uploaded.
+    #     # Set to a value such as 1000m; to restrict file size to a specific value
+    #     client_max_body_size 0;
+    #     # To disable buffering
+    #     proxy_buffering off;
+    #   '';
+    # };
+    # "s3.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   locations."/" = {
+    #     proxyPass = "http://${config.link.serviceHost}:9000";
+    #     extraConfig = ''
+    #       proxy_set_header X-Real-IP $remote_addr;
+    #       proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    #       proxy_set_header X-Forwarded-Proto $scheme;
+    #       # proxy_set_header Host $host;
+    #       proxy_connect_timeout 300;
+    #       # Default is HTTP/1, keepalive is only enabled in HTTP/1.1
+    #       # proxy_http_version 1.1;
+    #       proxy_set_header Connection "";
+    #       chunked_transfer_encoding off;
+    #     '';
+    #     proxyWebsockets = true;
+    #   };
+    #   extraConfig = ''
+    #     # To allow special characters in headers
+    #     ignore_invalid_headers off;
+    #     # Allow any size file to be uploaded.
+    #     # Set to a value such as 1000m; to restrict file size to a specific value
+    #     client_max_body_size 0;
+    #     # To disable buffering
+    #     proxy_buffering off;
+    #   '';
+    # };
+    # "diagrams.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   locations."/" = {
+    #     proxyPass = "http://${config.link.serviceHost}:8765";
+    #   };
+    #   # extraConfig = mkIf (!cfg.expose) ''
+    #   #   allow ${config.link.service-ip}/24;
+    #   #     allow 127.0.0.1;
+    #   #     deny all; # deny all remaining ips
+    #   # '';
+    # };
+    # "nextcloud.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   locations."/" = {
+    #     proxyPass = "http://${config.link.serviceHost}:80";
+    #   };
+    # };
+    # "outline.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   locations."/" = {
+    #     proxyPass = "http://${config.link.serviceHost}:${toString config.link.services.outline.port}";
+    #     proxyWebsockets = true;
+    #   };
+    # };
+    # "vaultwarden.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   locations."/" = {
+    #     proxyPass = "http://${config.link.serviceHost}:${toString config.link.services.vaultwarden.port}";
+    #     proxyWebsockets = true;
+    #   };
+    # };
+    # "photoprism.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   http2 = true;
+    #   locations."/" = {
+    #     proxyPass = "http://${config.link.serviceHost}:${toString config.link.services.photoprism.port}/";
+    #     proxyWebsockets = true;
+    #     # extraConfig = ''
+    #     #   proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    #     #   proxy_set_header Host $host;
+    #     #   proxy_buffering off;
+    #     #   proxy_http_version 1.1;
+    #     # '';
+    #   };
+    # };
+    # services.nginx.virtualHosts."paperless.${config.link.domain}" = {
+    #   enableACME = true;
+    #   forceSSL = true;
+    #   locations."/" = {
+    #     proxyPass = "http://${config.link.serviceHost}:${toString config.link.services.paperless.port}";
+    #     proxyWebsockets = true;
+    #   };
+    # };
+
+    ## CTF
+  #   "slides.netintro.${config.link.domain}" = {
+  #     enableACME = true;
+  #     forceSSL = true;
+  #     # default = true;
+  #     locations."/" = {
+  #       proxyPass = "http://100.89.40.41:31337/";
+  #     };
   #   };
-  # services.nginx.virtualHosts."diagrams.${config.link.domain}" = {
-  #   enableACME = true;
-  #   forceSSL = true;
-  #   locations."/" = {
-  #     proxyPass = "http://127.0.0.1:8765";
-  #     proxyWebsockets = true;
+  #   "vpnconfig.netintro.${config.link.domain}" = {
+  #     enableACME = true;
+  #     forceSSL = true;
+  #     # default = true;
+  #     locations."/" = {
+  #       proxyPass = "http://100.89.40.41:31338/";
+  #     };
   #   };
-  #   # extraConfig = mkIf (!cfg.expose) ''
-  #   #   allow ${config.link.service-ip}/24;
-  #   #     allow 127.0.0.1;
-  #   #     deny all; # deny all remaining ips
-  #   # '';
-  # };
-  services.nginx.virtualHosts."matrix.${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://${config.link.serviceHost}:${toString config.link.services.matrix.port}";
-    };
-  };
-  services.nginx.virtualHosts."gitea.${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://${config.link.serviceHost}:${toString config.services.gitea.settings.server.HTTP_PORT}";
-    };
-  };
-  services.nginx.virtualHosts."grafana.${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://${config.link.serviceHost
-            }:${toString config.link.services.grafana.port}/";
-      proxyWebsockets = true;
-    };
-  };
-  services.nginx.virtualHosts."hedgedoc.${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-
-    locations."/".proxyPass = "http://${config.link.serviceHost}:${toString config.link.services.hedgedoc.port}";
-
-  };
-  services.nginx.virtualHosts."jellyfin.${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    listen = [{ port = 443; addr = "0.0.0.0"; ssl = true; } { port = 8096; addr = "0.0.0.0"; ssl = true; } { port = 8920; addr = "0.0.0.0"; ssl = true; }];
-    locations."/".proxyPass = "http://${config.link.serviceHost}:8096/";
-  };
-  services.nginx.virtualHosts."jellyfin1.${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/".proxyPass = "http://${config.link.serviceHost}:8096/";
-  };
-  services.nginx.virtualHosts."jellyfin2.${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/".proxyPass = "http://${config.link.serviceHost}:8920/";
-  };
-  services.nginx.virtualHosts."jellyseer.${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/".proxyPass = "http://${config.link.serviceHost}:5055/";
-  };
-  services.  nginx.virtualHosts."minio.s3.${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://${config.link.serviceHost}:9001";
-      proxyWebsockets = true;
-      extraConfig = ''
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-        # proxy_set_header Host $host;
-        proxy_connect_timeout 300;
-        # Default is HTTP/1, keepalive is only enabled in HTTP/1.1
-        #proxy_http_version 1.1;
-        proxy_set_header Connection "";
-        chunked_transfer_encoding off;
-      '';
-    };
-    extraConfig = ''
-      # To allow special characters in headers
-      ignore_invalid_headers off;
-      # Allow any size file to be uploaded.
-      # Set to a value such as 1000m; to restrict file size to a specific value
-      client_max_body_size 0;
-      # To disable buffering
-      proxy_buffering off;
-    '';
-  };
-  services.nginx.virtualHosts."s3.${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://${config.link.serviceHost}:9000";
-      extraConfig = ''
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-        # proxy_set_header Host $host;
-        proxy_connect_timeout 300;
-        # Default is HTTP/1, keepalive is only enabled in HTTP/1.1
-        # proxy_http_version 1.1;
-        proxy_set_header Connection "";
-        chunked_transfer_encoding off;
-      '';
-      proxyWebsockets = true;
-    };
-    extraConfig = ''
-      # To allow special characters in headers
-      ignore_invalid_headers off;
-      # Allow any size file to be uploaded.
-      # Set to a value such as 1000m; to restrict file size to a specific value
-      client_max_body_size 0;
-      # To disable buffering
-      proxy_buffering off;
-    '';
-  };
-  services.nginx.virtualHosts."diagrams.${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://${config.link.serviceHost}:8765";
-    };
-    # extraConfig = mkIf (!cfg.expose) ''
-    #   allow ${config.link.service-ip}/24;
-    #     allow 127.0.0.1;
-    #     deny all; # deny all remaining ips
-    # '';
-  };
-  services.nginx.virtualHosts."nextcloud.${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://${config.link.serviceHost}:80";
-    };
-  };
-  services.nginx.virtualHosts."outline.${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://${config.link.serviceHost}:${toString config.link.services.outline.port}";
-      proxyWebsockets = true;
-    };
-  };
-  services.nginx.virtualHosts."vaultwarden.${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://${config.link.serviceHost}:${toString config.link.services.vaultwarden.port}";
-      proxyWebsockets = true;
-    };
-  };
-  services.nginx.virtualHosts."photoprism.${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    http2 = true;
-    locations."/" = {
-      proxyPass = "http://${config.link.serviceHost}:${toString config.link.services.photoprism.port}/";
-      proxyWebsockets = true;
-      # extraConfig = ''
-      #   proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-      #   proxy_set_header Host $host;
-      #   proxy_buffering off;
-      #   proxy_http_version 1.1;
-      # '';
-    };
-  };
-  # services.nginx.virtualHosts."paperless.${config.link.domain}" = {
-  #   enableACME = true;
-  #   forceSSL = true;
-  #   locations."/" = {
-  #     proxyPass = "http://${config.link.serviceHost}:${toString config.link.services.paperless.port}";
-  #     proxyWebsockets = true;
-  #   };
-  # };
-
-  ## CTF
-  services.nginx.virtualHosts."slides.netintro.${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    # default = true;
-    locations."/" = {
-      proxyPass = "http://100.89.40.41:31337/";
-    };
-  };
-  services.nginx.virtualHosts."vpnconfig.netintro.${config.link.domain}" = {
-    enableACME = true;
-    forceSSL = true;
-    # default = true;
-    locations."/" = {
-      proxyPass = "http://100.89.40.41:31338/";
-    };
   };
   ## /CTF
 
@@ -296,5 +314,6 @@
     ssh.host = "nc";
   };
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  nixpkgs.hostPlatform = lib.mkDefault
+    "x86_64-linux";
 }
