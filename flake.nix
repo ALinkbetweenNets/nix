2e0abdea3dff17c6dd16fcbc05045877df7443ca
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-05-11 20:17:41 +0200

    Revert "remove vscode-server (broken)"
    This reverts commit 0edf1b7b1adb6bac8c066439fe5b45d8290659b3.

--- flake.nix
+++ flake.nix
@@ -29,10 +29,10 @@
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # vscode-server = {
-    #   url = "github:msteen/nixos-vscode-server";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    vscode-server = {
+      url = "github:msteen/nixos-vscode-server";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     crab_share = {
       url = "github:lounge-rocks/crab_share";
     };
@@ -184,7 +184,7 @@
           # import home manager modules from this flake
           imports = [
             inputs.nixvim.homeManagerModules.nixvim
-            # inputs.vscode-server.nixosModules.home
+            inputs.vscode-server.nixosModules.home
           ];
           # add overlays from this flake
           nixpkgs.overlays = [
@@ -199,10 +199,10 @@
             })
           ];
           # Visual Studio Code Server support
-          # services.vscode-server = {
-          #   enable = true;
-          #   installPath = "~/.vscode-server";
-          # };
+          services.vscode-server = {
+            enable = true;
+            installPath = "~/.vscode-server";
+          };
         };
       };
     };
