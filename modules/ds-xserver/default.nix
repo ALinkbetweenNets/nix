e30493be352ecd91d317ea77d38a68fdfebbbdcf
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-05-21 12:43:32 +0200

    libinput rename

--- modules/ds-xserver/default.nix
+++ modules/ds-xserver/default.nix
@@ -5,28 +5,30 @@ in {
   options.link.xserver.enable = mkEnableOption "activate xserver";
   config = mkIf cfg.enable {
     # Enable the X11 windowing system.
-    services.xserver = {
-      enable = true;
-      autorun = true;
-      xkb = {
-        layout = "de";
-        variant = "nodeadkeys";
-        options = "eurosign:e,caps:escape";
-      };
+    services = {
       libinput = {
         enable = true;
         touchpad.accelProfile = "adaptive";
       };
-      desktopManager = {
-        xterm.enable = false;
-        session = [{
-          name = "home-manager";
-          start = ''
-            export `dbus-launch`
-            ${pkgs.runtimeShell} $HOME/.hm-xsession &
-             waitPID=$!
-          '';
-        }];
+      xserver = {
+        enable = true;
+        autorun = true;
+        xkb = {
+          layout = "de";
+          variant = "nodeadkeys";
+          options = "eurosign:e,caps:escape";
+        };
+        desktopManager = {
+          xterm.enable = false;
+          session = [{
+            name = "home-manager";
+            start = ''
+              export `dbus-launch`
+              ${pkgs.runtimeShell} $HOME/.hm-xsession &
+               waitPID=$!
+            '';
+          }];
+        };
       };
     };
     # Enable pulseaudio compatible api for audio volume control in i3
