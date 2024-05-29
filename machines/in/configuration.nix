f0d62386a2b3c76e2020cee0efa39bd2d9ab7e0a
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-05-29 18:11:06 +0200

    in remove lollypops host, remove steam

--- machines/in/configuration.nix
+++ machines/in/configuration.nix
@@ -13,11 +13,10 @@
     plasma.enable = false;
     gnome.enable = true;
   };
-  programs.steam.enable = lib.mkForce false;
   services.smartd.enable = false;
   networking.hostName = "in";
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-  lollypops.deployment = { local-evaluation = true; };
+  lollypops.deployment = { local-evaluation = true; ssh.host = "192.168.178.65"; };
   environment.systemPackages = with pkgs;    [ ];
   #system.stateVersion = "23.05";
 }
