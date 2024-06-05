57284f0c13ff567f33c5ca3cce0b60f4df7a3ef1
ALinkbetweenNets <--global>
2024-06-05 15:24:42 +0200

    gaming

---
+++ modules/common/gaming.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, lib, ... }:
+with lib;
+let cfg = config.link.gaming;
+in {
+  options.link.gaming = { enable = mkEnableOption "activate gaming"; };
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      looking-glass-client # KVM relay
+      # (wine.override { wineBuild = "wine64"; })
+      # wineWowPackages.staging
+      # winetricks
+      #wine
+      #wineWowPackages.waylandFull
+    ];
+    programs = {
+      steam.enable = true;
+      steam.gamescopeSession.enable = true;
+    };
+  };
+}
