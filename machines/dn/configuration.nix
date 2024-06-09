535b617e2e0d05e48c3c1a81512cd7e926cfdf5d
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-06-09 13:53:26 +0200

    unifi update

--- machines/dn/configuration.nix
+++ machines/dn/configuration.nix
@@ -80,7 +80,7 @@
       local all all trust
     '';
   };
-  # services.unifi = { enable = true; openFirewall = true; };
+  services.unifi = { enable = true; openFirewall = true; unifiPackage = pkgs.unifi; };
   networking = {
     hostName = "dn";
     domain = "monitor-banfish.ts.net";
