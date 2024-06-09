4c35a86618082f8a9369c11940b13ae0118983e3
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-06-09 14:07:00 +0200

    paperless address fix

--- modules/services/paperless.nix
+++ modules/services/paperless.nix
@@ -30,10 +30,9 @@ in {
     services = {
       paperless = {
         enable = true;
-        #address=if cfg.expose-port then "0.0.0.0" else ""127.0.0.1";
+        address = if cfg.expose-port then "0.0.0.0" else "127.0.0.1";
         passwordFile = config.sops.secrets."paperless".path;
         #dataDir =
-        address = "paperless.alinkbetweennets.de";
         port = cfg.port;
         settings = {
           PAPERLESS_ADMIN_USER = "l";
