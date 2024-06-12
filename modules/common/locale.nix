b95a6d9d3afec33ca9da66b93d165f401037e5e0
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-06-12 13:45:03 +0200

    locale fix

--- modules/common/locale.nix
+++ modules/common/locale.nix
@@ -4,19 +4,21 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
-    LANGUAGE = "en_US.UTF-8";
-    LANG = "en_US.UTF-8";
+    # LANGUAGE = "en_US.UTF-8";
+    # LANG = "en_US.UTF-8";
     LC_ADDRESS = "de_DE.UTF-8";
+    LC_COLLATE = "de_DE.UTF-8";
+    LC_CTYPE = "de_DE.UTF-8";
     LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MESSAGES = "en_US.UTF-8";
     LC_MONETARY = "de_DE.UTF-8";
+    LC_MESSAGES = "en_US.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
     LC_NAME = "de_DE.UTF-8";
     LC_NUMERIC = "de_DE.UTF-8";
     LC_PAPER = "de_DE.UTF-8";
     LC_TELEPHONE = "de_DE.UTF-8";
     LC_TIME = "en_DK.UTF-8";
-    LC_ALL = "en_US.UTF-8";
+    # LC_ALL = "en_US.UTF-8";
   };
   # Configure console keymap
   console.keyMap = "de";
