5161705ea154b75e118b5659656313db0c86ebb4
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-06-17 23:17:11 +0200

    nc immich, gitlab port fix

--- machines/nc/configuration.nix
+++ machines/nc/configuration.nix
@@ -121,7 +121,7 @@
     enableACME = true;
     forceSSL = true;
     locations."/" = {
-      proxyPass = "http://${config.link.serviceHost}:${toString config.link.services.gitlab.port}/";
+      proxyPass = "http://${config.link.serviceHost}:80/";
       proxyWebsockets = true;
     };
   };
@@ -155,6 +155,11 @@
     enableACME = true;
     forceSSL = true;
     locations."/".proxyPass = "http://${config.link.serviceHost}:2500/";
+  };
+  services.nginx.virtualHosts."immich.${config.link.domain}" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/".proxyPass = "http://10.10.10.89:2283/";
   };
   services.nginx.virtualHosts."kinky3d.de" = {
     enableACME = true;
