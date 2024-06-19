779ccbbba022c8065516258af82714aebdbf4e8d
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-06-19 10:27:16 +0200

    xn key change

--- modules/users/l.nix
+++ modules/users/l.nix
@@ -19,7 +19,7 @@ in {
       ;
       shell = "${pkgs.zsh}/bin/zsh";
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/1eePE6/pYo4aahzcDRqbmnVdx9ikKH+93yw7M1pXH l@xn"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM0skthdvhy0oNOD/nbZ4H6tcMOffiNiRlquVTtdZIy/ l@xn"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINI74luZ3xJcgaZYHzn5DtSpYufml+SbhZQV12gWGShS l@dn"
       ];
       hashedPasswordFile = config.sops.secrets."users/l/hashedPassword".path; # Initial password
