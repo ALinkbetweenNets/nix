ba1d04cc959df61de053275d6b31f0c611b387c6
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-06-21 00:03:35 +0200

    xn reenable disko

--- machines/xn/hardware-configuration-x390.nix
+++ machines/xn/hardware-configuration-x390.nix
@@ -21,14 +21,14 @@
       #   "/crypto_keyfile.bin" = null;
       # };
       luks.devices = {
-        "luks-1e4c0964-e0dc-482c-a999-64ee1cc3725d" = {
-          device = "/dev/disk/by-uuid/854c679d-ad2a-450b-830c-fd49633cbd31";
+        "root" = {
+          device = "/dev/nvme0n1p2";
           #keyFile="/crypto_keyfile.bin";
           #preLVM = true;
           #allowDiscards = true;
         };
-        "luks-b6df9624-aab6-4d59-ac03-817bbb806b6c" = {
-          device = "/dev/disk/by-uuid/4473a751-a85a-448f-bacf-e821bb543be4";
+        "swap" = {
+          device = "/dev/nvme0n1p3";
           #keyFile="/crypto_keyfile.bin";
         };
       };
@@ -36,16 +36,16 @@
   };
   fileSystems."/" =
     {
-      device = "/dev/disk/by-uuid/854c679d-ad2a-450b-830c-fd49633cbd31";
+      device = "/dev/mapper/root";
       fsType = "ext4";
     };
   fileSystems."/boot" =
     {
-      device = "/dev/disk/by-uuid/6F02-2160";
+      device = "/dev/nvme0n1p1";
       fsType = "vfat";
     };
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/4473a751-a85a-448f-bacf-e821bb543be4"; }];
+    [{ device = "/dev/mapper/swap"; }];
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
