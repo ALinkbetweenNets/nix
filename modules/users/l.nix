d289353bb9d05ed372b2706bb2cef764d227356c
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-07-08 14:57:39 +0200

    x390 ssh key

--- modules/users/l.nix
+++ modules/users/l.nix
@@ -20,6 +20,7 @@ in {
       shell = "${pkgs.zsh}/bin/zsh";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOaLOyxsr6wgj0JoG/OrDywND2hG2nblOGUuZBPFG1U l@xn"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM0skthdvhy0oNOD/nbZ4H6tcMOffiNiRlquVTtdZIy/ l@x390"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINI74luZ3xJcgaZYHzn5DtSpYufml+SbhZQV12gWGShS l@dn"
       ];
       hashedPasswordFile = mkIf config.link.sops config.sops.secrets."users/l/hashedPassword".path; # Initial password
