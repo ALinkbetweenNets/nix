e55d8abf33f8c82239c4e553c25603aad7b7b403
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-07-15 10:37:41 +0200

    ssh now only on tailscale

--- modules/openssh/default.nix
+++ modules/openssh/default.nix
@@ -4,22 +4,19 @@ let cfg = config.link.openssh;
 in {
   options.link.openssh.enable = mkEnableOption "activate openssh";
   config = mkIf cfg.enable {
-    networking.firewall.allowedTCPPorts = [ 2522 ];
+    # networking.firewall.allowedTCPPorts = [ 2522 ];
+    networking.firewall.interfaces."tailscale0".allowedTCPPorts = mkIf config.link.tailscale.enable [ 2522 ];
+    networking.firewall.allowedTCPPorts = mkIf (!config.link.sops || !config.link.tailscale.enable) [ 2522 ];
     # Enable the OpenSSH daemon.
     services.openssh = {
       enable = true;
-      openFirewall = true;
+      openFirewall = false;
       startWhenNeeded = true;
       listenAddresses = [
         {
           addr = "0.0.0.0";
           port = 2522;
         }
-        # ] ++ lib.optionals (config.link.tailscale-address != "") [
-        #   {
-        #     addr = config.link.tailscale-address;
-        #     port = 22;
-        #   }
       ];
       settings = {
         LogLevel = "VERBOSE"; # for fail2ban to work properly
