9b064a2eb29f4f37fcaac3db0ea66dc553cff121
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-08-06 12:10:59 +0200

    zsh option rename

--- home-manager/modules/shell/default.nix
+++ home-manager/modules/shell/default.nix
@@ -67,7 +67,7 @@ in
     };
     zsh = {
       enable = true;
-      enableAutosuggestions = true;
+      autosuggestion.enable = true;
       enableCompletion = true;
       autocd = true;
       dotDir = ".config/zsh";
