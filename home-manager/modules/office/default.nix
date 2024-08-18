7aa6cb10f01622a4bf6287f1ded2f90e2c2db42e
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-08-18 11:24:38 +0200

    typst

--- home-manager/modules/office/default.nix
+++ home-manager/modules/office/default.nix
@@ -16,7 +16,7 @@ in {
   config = mkIf cfg.enable {
     programs = { };
     home.packages = with pkgs; [
-      beekeeper-studio # RDB
+      typst # better latex for papers
       libreoffice-qt
       gimp
       inkscape
