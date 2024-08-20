e6aaa5a4ab4073705d9fbd17146d16b6eec11061
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-08-20 11:55:54 +0200

    dns vpn fix

--- modules/services/dns.nix
+++ modules/services/dns.nix
@@ -4,27 +4,29 @@ let cfg = config.link.dns;
 in {
   options.link.dns.enable = mkEnableOption "activate dns";
   config = mkIf cfg.enable {
-    # networking.resolvconf.useLocalResolver = true;
+    networking.resolvconf.useLocalResolver = true;
     networking.networkmanager.enable = true;
-    networking.nameservers = [
-      # "127.0.0.1"
-      "9.9.9.9"
-      "192.168.250.1"
-      "192.168.150.1"
-      "100.100.100.100"
-      "194.242.2.2"
-      "1.0.0.1"
-    ];
+    networking.networkmanager.dns = "systemd-resolved";
+    networking.search = [ "local" "monitor-banfish.ts.net" ];
+    # networking.nameservers = [
+    #   # "127.0.0.1"
+    #   "9.9.9.9"
+    #   "192.168.250.1"
+    #   "192.168.150.1"
+    #   "100.100.100.100"
+    #   "194.242.2.2"
+    #   "1.0.0.1"
+    # ];
     services.resolved = {
       enable = true;
       fallbackDns = [
-        "192.168.150.1"
+        # "192.168.150.1" # sudo systemd-resolve --interface tun0 --set-dns 192.168.150.1
         "9.9.9.9"
-        # "127.0.0.1"
         "194.242.2.2"
-        "100.100.100.100"
         "1.0.0.1"
       ];
+      dnssec = "allow-downgrade";
+      dnsovertls = "opportunistic";
     };
     # services.dnsmasq = {
     #   enable = true;
