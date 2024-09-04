6716925340b995c81976ae552a53a28fba03dacd
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-09-04 16:46:18 +0200

    cpu-amd nvtop, nixgl

--- modules/hardware/cpu-amd.nix
+++ modules/hardware/cpu-amd.nix
@@ -1,9 +1,13 @@
-{ lib, pkgs, config, ... }:
+{ lib, pkgs, config, nixgl,... }:
 with lib;
 let cfg = config.link.cpu-amd;
 in {
   options.link.cpu-amd.enable = mkEnableOption "activate cpu-amd";
   config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      nvtopPackages.amd
+      nixgl
+    ];
     boot.extraModprobeConfig = "options kvm_amd nested=1";
     hardware.cpu.amd.updateMicrocode = true;
     boot.kernelParams = [ "amd_iommu=on" ];
