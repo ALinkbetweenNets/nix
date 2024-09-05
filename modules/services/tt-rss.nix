80c94f41f297f49ff73e33880ccf5aca8d1982d9
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-09-05 22:31:57 +0200

    disable tt-rss fully due to syntax errors

--- modules/services/tt-rss.nix
+++ modules/services/tt-rss.nix
@@ -28,41 +28,56 @@ in {
   };
   config = mkIf cfg.enable {
     services = {
-      tt-rss = {
-        enable = true;
-        pubSubHubbub.enable = true;
-        selfUrlPath = "https://rss.${config.link.domain}";
-        virtualHost = null;
-        logDestination = "syslog";
-      };
-      services.nginx = {
-        enable = lib.mkForce true;
-        virtualHosts = {
-          "rss.${config.link.domain} " = {
-            listen = [{
-              port = cfg.port;
-              addr = "0.0.0.0";
-              ssl = false;
-            }];
-            root = "${cfg.root}/www";
+      # tt-rss = {
+      #   enable = true;
+      #   pubSubHubbub.enable = true;
+      #   selfUrlPath = "https://rss.${config.link.domain}";
+      #   virtualHost = null;
+      #   logDestination = "syslog";
+      # };
+      # services.nginx = {
+      #   enable = lib.mkForce true;
+      #   virtualHosts = {
+      #     "rss.${config.link.domain} " = {
+      #       listen = [{
+      #         port = cfg.port;
+      #         addr = "0.0.0.0";
+      #         ssl = false;
+      #       }];
+      #       root = "/var/lib/tt-rss/www";
 
-            locations."/" = { index = "index.php"; };
+      #       locations."/" = { index = "index.php"; };
 
-            locations."^~ /feed-icons" = { root = "${cfg.root}"; };
+      #       locations."^~ /feed-icons" = { root = "/var/lib/tt-rss/"; };
 
-            locations."~ \\.php$" = {
-              extraConfig = ''
-                fastcgi_split_path_info ^(.+\.php)(/.+)$;
-                fastcgi_pass unix:${
-                  config.services.phpfpm.pools.${cfg.pool}.socket
-                };
-                fastcgi_index index.php;
-              '';
-            };
-          };
-        };
-
-      };
+      #       locations."~ \\.php$" = {
+      #         extraConfig = ''
+      #           fastcgi_split_path_info ^(.+\.php)(/.+)$;
+      #           fastcgi_pass unix:${
+      #             config.services.phpfpm.pools."tt-rss".socket
+      #           };
+      #           fastcgi_index index.php;
+      #         '';
+      #       };
+      #     };
+      #   };
+      # };
+      # services.phpfpm.pools."tt-rss" = {
+      #   #  inherit (cfg) user;
+      #   inherit phpPackage;
+      #   settings = mapAttrs (name: mkDefault) {
+      #     "listen.owner" = "nginx";
+      #     "listen.group" = "nginx";
+      #     "listen.mode" = "0600";
+      #     "pm" = "dynamic";
+      #     "pm.max_children" = 75;
+      #     "pm.start_servers" = 10;
+      #     "pm.min_spare_servers" = 5;
+      #     "pm.max_spare_servers" = 20;
+      #     "pm.max_requests" = 500;
+      #     "catch_workers_output" = 1;
+      #   };
+      # };
     };
   };
 }
