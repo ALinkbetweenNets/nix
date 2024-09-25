bb76e41b7e8b510d7dc2fce2835a3be97a4572aa
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-09-25 11:14:55 +0200

    amd_pstate

--- modules/hardware/cpu-amd.nix
+++ modules/hardware/cpu-amd.nix
@@ -10,7 +10,7 @@ in {
     ];
     boot.extraModprobeConfig = "options kvm_amd nested=1";
     hardware.cpu.amd.updateMicrocode = true;
-    boot.kernelParams = [ "amd_iommu=on" ];
+    boot.kernelParams = [ "amd_iommu=on" "amd_pstate=active" ];
     boot.kernelModules = [ "kvm-amd" ];
   };
 }
