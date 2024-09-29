9d9ead20545324a17c6b768a66772e32c01f18a3
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-09-29 16:23:29 +0200

    neovim removed autoCmd

--- home-manager/modules/neovim/default.nix
+++ home-manager/modules/neovim/default.nix
@@ -313,40 +313,40 @@ with lib; {
       #  # };
 
     };
-    autoCmd = [
-      # Vertically center document when entering insert mode
-      # {
-      #   event = "InsertEnter";
-      #   command = "norm zz";
-      # }      # Remove trailing whitespace on save
-      {
-        event = "BufWrite";
-        command = "%s/\\s\\+$//e";
-      } # Open help in a vertical split
-      # {
-      #   event = "FileType";
-      #   pattern = "help";
-      #   command = "wincmd L";
-      # }      # Set indentation to 2 spaces for nix files
-      {
-        event = "FileType";
-        pattern = "nix";
-        command = "setlocal tabstop=2 shiftwidth=2";
-      } # {
-      #   event = "FileType";
-      #   pattern = "oil";
-      #   callback = ''
-      #     function()
-      #       vim.keymap.set("n", "<leader>o", ":bd<cr>", { desc = "close oil", buffer = true })
-      #     end
-      #   '';
-      # }      # Enable spellcheck for some filetypes
-      {
-        event = "FileType";
-        pattern = [ "tex" "latex" "markdown" "norg" "text" ];
-        command = "setlocal spell spelllang=en";
-      }
-    ];
+    # autoCmd = [
+    #   # Vertically center document when entering insert mode
+    #   # {
+    #   #   event = "InsertEnter";
+    #   #   command = "norm zz";
+    #   # }      # Remove trailing whitespace on save
+    #   {
+    #     event = "BufWrite";
+    #     command = "%s/\\s\\+$//e";
+    #   } # Open help in a vertical split
+    #   # {
+    #   #   event = "FileType";
+    #   #   pattern = "help";
+    #   #   command = "wincmd L";
+    #   # }      # Set indentation to 2 spaces for nix files
+    #   {
+    #     event = "FileType";
+    #     pattern = "nix";
+    #     command = "setlocal tabstop=2 shiftwidth=2";
+    #   } # {
+    #   #   event = "FileType";
+    #   #   pattern = "oil";
+    #   #   callback = ''
+    #   #     function()
+    #   #       vim.keymap.set("n", "<leader>o", ":bd<cr>", { desc = "close oil", buffer = true })
+    #   #     end
+    #   #   '';
+    #   # }      # Enable spellcheck for some filetypes
+    #   {
+    #     event = "FileType";
+    #     pattern = [ "tex" "latex" "markdown" "norg" "text" ];
+    #     command = "setlocal spell spelllang=en";
+    #   }
+    # ];
   };
   home.packages = with pkgs; [
     lua-language-server
