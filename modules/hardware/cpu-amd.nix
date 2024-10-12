865a3825dc510b32be8066757dd755803a75c1c5
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-10-13 00:10:50 +0200

    move hardware.graphics 32 bit to amd

--- modules/hardware/cpu-amd.nix
+++ modules/hardware/cpu-amd.nix
@@ -1,16 +1,18 @@
-{ lib, pkgs, config, nixgl,... }:
+{ lib, pkgs, config, nixgl, ... }:
 with lib;
 let cfg = config.link.cpu-amd;
 in {
   options.link.cpu-amd.enable = mkEnableOption "activate cpu-amd";
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      nvtopPackages.amd
-      nixgl
-    ];
+    environment.systemPackages = with pkgs; [ nvtopPackages.amd nixgl ];
     boot.extraModprobeConfig = "options kvm_amd nested=1";
     hardware.cpu.amd.updateMicrocode = true;
     boot.kernelParams = [ "amd_iommu=on" "amd_pstate=active" ];
     boot.kernelModules = [ "kvm-amd" ];
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = with pkgs; [ vaapiVdpau libvdpau-va-gl ];
+    };
   };
 }
