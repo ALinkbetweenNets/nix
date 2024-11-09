968ad0a35d9a21414dc8d7b50fc6e432948b2f09
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-11-09 10:51:16 +0100

    fn kubo

--- machines/fn/configuration.nix
+++ machines/fn/configuration.nix
@@ -66,7 +66,9 @@
       ];
     };
   };
+  users.users.l.extraGroups = [ config.services.kubo.group ];
   services = {
+    kubo.enable = true;
     # ollama = {
     #   enable = true;
     #   port = 11434;
