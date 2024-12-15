bce3514347fc3d02d5f92167d4a9b8aee436e712
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-12-16 00:46:00 +0100

    podman

--- modules/users/l.nix
+++ modules/users/l.nix
@@ -4,9 +4,7 @@ let cfg = config.link.users.l;
 in {
   options.link.users.l = { enable = mkEnableOption "activate user l"; };
   config = mkIf cfg.enable {
-    users.groups = {
-      wireshark={};
-    };
+    users.groups = { wireshark = { }; };
     users.users.l = {
       isNormalUser = true;
       home = "/home/l";
@@ -25,7 +23,8 @@ in {
         [ "networkmanager" ]
         ++ lib.optionals config.link.printing.enable [ "scanner" "lp" ]
         ++ lib.optionals config.link.libvirt.enable [ "libvirtd" "kvm" ]
-        ++ lib.optionals config.link.docker.enable [ "docker" ];
+        ++ lib.optionals config.link.docker.enable [ "docker" ]
+        ++ lib.optionals config.link.podman.enable [ "podman" ];
       shell = "${pkgs.zsh}/bin/zsh";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOaLOyxsr6wgj0JoG/OrDywND2hG2nblOGUuZBPFG1U l@xn"
