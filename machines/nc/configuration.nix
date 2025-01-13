b5158b1a3fbee6bb8679f376d8b9c9125f7f8cae
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-01-13 12:34:42 +0100

    nc new service host

--- machines/nc/configuration.nix
+++ machines/nc/configuration.nix
@@ -13,7 +13,7 @@
     domain = "alinkbetweennets.de";
     fail2ban.enable = true;
     nginx.enable = true;
-    serviceHost = "100.122.145.19";
+    serviceHost = "100.108.233.76";
     server.enable = true;
     vm.enable = true;
     # services.coturn.enable = true;
