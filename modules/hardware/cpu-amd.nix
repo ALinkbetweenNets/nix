d3a20b851934012737da13c7435b987c0b5865b1
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-02-08 13:08:38 +0100

    fix amd igpu drivers

--- modules/hardware/cpu-amd.nix
+++ modules/hardware/cpu-amd.nix
@@ -5,28 +5,33 @@ in {
   options.link.cpu-amd.enable = mkEnableOption "activate cpu-amd";
   config = mkIf cfg.enable {
     services.xserver.videoDrivers = [ "amdgpu" ];
-    environment.systemPackages = with pkgs; [ nvtopPackages.amd ];
+    environment.systemPackages = with pkgs; [ nvtopPackages.amd clinfo ];
     boot = {
       kernelParams = [
         "amd_iommu=on"
         "amd_pstate=active"
-        "amdgpu.dcfeaturemask=0x8"
-        "amdgpu.sg_display=0"
+        # "amdgpu.dcfeaturemask=0x8"
+        # "amdgpu.sg_display=0"
+        #"radeon.cik_support=0"
+        # "amdgpu.cik_support=1"
       ];
       extraModprobeConfig = "options kvm_amd nested=1";
       kernelModules = [ "kvm-amd" "amdgpu" ];
     };
     hardware = {
       cpu.amd.updateMicrocode = true;
+      # amdgpu.initrd.enable = lib.mkDefault true;
       graphics = {
         enable = true;
         enable32Bit = true;
-        # extraPackages = with pkgs; [
-        #   amdvlk
-        #   # rocmPackages.clr.icd
-        #   # rocm-opencl-runtime
-        # ];
-        # extraPackages = with pkgs; [ vaapiVdpau libvdpau-va-gl ];
+        extraPackages = with pkgs; [
+          # amdvlk
+          # rocmPackages.clr.icd
+          # rocmPackages.clr
+          # mesa.opencl
+          vaapiVdpau
+          libvdpau-va-gl
+        ];
       };
     };
   };
