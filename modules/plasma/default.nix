72a1a9dae9e57c4442f998a050b02355e5c776f0
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-03-13 12:46:39 +0100

    sddm-kcm

--- modules/plasma/default.nix
+++ modules/plasma/default.nix
@@ -5,10 +5,17 @@ in {
   options.link.plasma.enable = mkEnableOption "activate plasma";
   config = mkIf cfg.enable {
     services = {
-      displayManager.sddm.enable = true;
-      # desktopManager.plasma5.useQtScaling = true;
+      displayManager.sddm = {
+        enable = true;
+        # wayland.enable = true;
+        # theme = "/home/l/aerial-sddm-theme/";
+        # theme= "breeze";
+      };
       desktopManager.plasma6.enable = true;
     };
-    environment.systemPackages = with pkgs; [ kdePackages.plasma-nm ];
+    environment.systemPackages = with pkgs; [
+      kdePackages.sddm-kcm # sddm config module
+      kdePackages.plasma-nm
+    ];
   };
 }
