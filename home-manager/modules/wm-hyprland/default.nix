e4e9cf2268a7fcb1c7f715a563d4e37cd315d447
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-03-13 13:06:18 +0100

    disable hyprland specific services for now

--- home-manager/modules/wm-hyprland/default.nix
+++ home-manager/modules/wm-hyprland/default.nix
@@ -16,35 +16,35 @@ in {
     # };
     services = {
       # Networking
-      network-manager-applet.enable = true;
-      # Bluetooth
-      blueman.enable = true;
-      # Pulseaudio
-      pasystray.enable = true;
-      # Battery Warning
-      cbatticon.enable = true;
-      hypridle = {
-        enable = true;
-        settings = {
-          general = {
-            after_sleep_cmd = "hyprctl dispatch dpms on";
-            ignore_dbus_inhibit = false;
-            lock_cmd = "hyprlock";
-          };
+      # network-manager-applet.enable = true;
+      # # Bluetooth
+      # blueman.enable = true;
+      # # Pulseaudio
+      # pasystray.enable = true;
+      # # Battery Warning
+      # cbatticon.enable = true;
+      # hypridle = {
+      #   enable = true;
+      #   settings = {
+      #     general = {
+      #       after_sleep_cmd = "hyprctl dispatch dpms on";
+      #       ignore_dbus_inhibit = false;
+      #       lock_cmd = "hyprlock";
+      #     };
 
-          listener = [
-            {
-              timeout = 900;
-              on-timeout = "hyprlock";
-            }
-            {
-              timeout = 1200;
-              on-timeout = "hyprctl dispatch dpms off";
-              on-resume = "hyprctl dispatch dpms on";
-            }
-          ];
-        };
-      };
+      #     listener = [
+      #       {
+      #         timeout = 900;
+      #         on-timeout = "hyprlock";
+      #       }
+      #       {
+      #         timeout = 1200;
+      #         on-timeout = "hyprctl dispatch dpms off";
+      #         on-resume = "hyprctl dispatch dpms on";
+      #       }
+      #     ];
+      #   };
+      # };
     };
     programs = {
       fuzzel = {
