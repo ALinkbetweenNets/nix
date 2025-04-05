6d59b6ba6926b4a2a2ce4b477abbd744f02f5e58
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-04-05 19:07:26 +0200

    sn npn key

--- machines/sn/configuration.nix
+++ machines/sn/configuration.nix
@@ -136,6 +136,7 @@
   # };
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBu+WcpENdr7FaCIwj6WsinGnykIPV/tnIyrfEHSeU+E root@sn"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM1CM1Z6Q6qbzWhHiUlOtZ0UQjkCUBj6dg5YAik4sw4+ root@npn"
   ];
   nix.settings.auto-optimise-store = true;
   # services.cloudflare-dyndns = {
