7f58696569b929e4d8cc4ec0b69c0a5535d06768
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-04-13 14:13:21 +0200

    remove lsplines

--- home-manager/modules/neovim/default.nix
+++ home-manager/modules/neovim/default.nix
@@ -18,7 +18,7 @@ with lib; {
           enable = true;
           formatOnSave = false;
           lightbulb.enable = true;
-          lsplines.enable = true;
+          # lsplines.enable = true;
           lspsaga.enable = false;
           lspSignature.enable = true;
           nvim-docs-view.enable = true;
