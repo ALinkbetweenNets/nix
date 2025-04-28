330271214372fd70990d8cb8fc1c5342ad228690
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-04-28 10:42:10 +0200

    prometheus

--- modules/services/prometheus.nix
+++ modules/services/prometheus.nix
@@ -20,11 +20,163 @@ in {
       prometheus = {
         enable = true;
         port = cfg.port;
+        scrapeConfigs = [{
+          job_name = "scraper";
+          static_configs = [{
+            targets = [
+              "127.0.0.1:${
+                toString config.services.prometheus.exporters.node.port
+              }"
+              "127.0.0.1:${
+                toString config.services.prometheus.exporters.ping.port
+              }"
+              "127.0.0.1:${
+                toString config.services.prometheus.exporters.domain.port
+              }"
+              "127.0.0.1:${
+                toString config.services.prometheus.exporters.postgres.port
+              }"
+              "127.0.0.1:${
+                toString config.services.prometheus.exporters.redis.port
+              }"
+              # "127.0.0.1:${
+              # toString config.services.prometheus.exporters.restic.port
+              # }"
+              "127.0.0.1:${
+                toString config.services.prometheus.exporters.statsd.port
+              }"
+              "127.0.0.1:${
+                toString config.services.prometheus.exporters.smokeping.port
+              }"
+              # "127.0.0.1:${
+              #   toString config.services.prometheus.exporters.blackbox.port
+              # }"
+
+            ];
+          }];
+        }];
+
         exporters = {
-          zfs = {
+          node = {
             enable = true;
-            listenAddress = "0.0.0.0";
+            enabledCollectors = [
+              "systemd"
+              "cpu"
+              "diskstats"
+              "filesystem"
+              "loadavg"
+              "meminfo"
+              "netdev"
+              "stat"
+              "time"
+            ];
           };
+          ping = {
+            enable = true;
+            settings = {
+              targets = [ "1.0.0.1" ];
+              interval = "20s";
+              timeout = "5s";
+              history-size = "120";
+              payload-size = "64";
+            };
+          };
+          domain.enable = true;
+          postgres.enable = true;
+          redis.enable = true;
+          # restic.enable = true;
+          statsd.enable = true;
+          smokeping = {
+            enable = true;
+            hosts = [ "1.0.0.1" ];
+          };
+          # blackbox = {
+          #   enable = true;
+          #   configFile = builtins.toFile "blackbox.yml" ''
+          #     modules:
+          #       http_2xx:
+          #         prober: http
+          #         timeout: 5s
+          #         http:
+          #       http_post_2xx:
+          #         prober: http
+          #         timeout: 5s
+          #         http:
+          #           method: POST
+          #           body_size_limit: 1MB
+          #       tcp_connect:
+          #         prober: tcp
+          #         timeout: 5s
+          #       pop3s_banner:
+          #         prober: tcp
+          #         tcp:
+          #           query_response:
+          #           - expect: "^+OK"
+          #           tls: true
+          #           tls_config:
+          #             insecure_skip_verify: false
+          #       ssh_banner:
+          #         prober: tcp
+          #         timeout: 5s
+          #         tcp:
+          #           query_response:
+          #           - expect: "^SSH-2.0-"
+          #       smtp_starttls:
+          #         prober: tcp
+          #         timeout: 5s
+          #         tcp:
+          #           query_response:
+          #           - expect: "^220 "
+          #           - send: "EHLO prober\r"
+          #           - expect: "^250-STARTTLS"
+          #           - send: "STARTTLS\r"
+          #           - expect: "^220"
+          #           - starttls: true
+          #           - send: "EHLO prober\r"
+          #           - expect: "^250-AUTH"
+          #           - send: "QUIT\r"
+          #       irc_banner:
+          #         prober: tcp
+          #         timeout: 5s
+          #         tcp:
+          #           query_response:
+          #           - send: "NICK prober"
+          #           - send: "USER prober prober prober :prober"
+          #           - expect: "PING :([^ ]+)"
+          #             send: "PONG ${1}"
+          #           - expect: "^:[^ ]+ 001"
+          #       icmp_test:
+          #         prober: icmp
+          #         timeout: 5s
+          #         icmp:
+          #           preferred_ip_protocol: ip4
+          #       dns_test:
+          #         prober: dns
+          #         timeout: 5s
+          #         dns:
+          #           query_name: example.com
+          #           preferred_ip_protocol: ip4
+          #           ip_protocol_fallback: false
+          #           validate_answer_rrs:
+          #             fail_if_matches_regexp: [test]
+          #       http_header_match_origin:
+          #         prober: http
+          #         timeout: 5s
+          #         http:
+          #           method: GET
+          #           headers:
+          #             Origin: example.com
+          #           fail_if_header_not_matches:
+          #           - header: Access-Control-Allow-Origin
+          #             regexp: '(\*|example\.com)'
+          #             allow_missing: false
+          #   '';
+          # };
+
+          # zfs = {
+          #   enable = true;
+          #   listenAddress = "0.0.0.0";
+          # };
           # nextcloud={
           #   enable=true;
           #   username="l";
@@ -32,14 +184,13 @@ in {
           #   port="443";
           # };
         };
-        scrapeConfigs = [{
-          job_name = "zfs";
-          static_configs =
-            [{ targets = [ "127.0.0.1:${toString cfg.port}" ]; }];
-        }];
+        # scrapeConfigs = [{
+        #   job_name = "zfs";
+        #   static_configs =
+        #     [{ targets = [ "127.0.0.1:${toString cfg.port}" ]; }];
+        # }];
       };
     };
-
     networking.firewall.interfaces."${config.link.service-interface}".allowedTCPPorts =
       mkIf cfg.expose-port [ cfg.port ];
   };
