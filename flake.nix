e742288a107c151e01f5883ba461dcddfacc02f0
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-05-27 14:58:44 +0200

    lix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   description = "My NixOS infrastructure";
   inputs = {
+    lix-module = {
+      url =
+        "https://git.lix.systems/lix-project/nixos-module/archive/2.93.0.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
