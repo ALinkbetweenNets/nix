37b34014b54ccf813b2bbe6566c89742039da0a5
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-06-02 23:47:33 +0200

    netbox nginx

--- modules/services/netbox.nix
+++ modules/services/netbox.nix
@@ -32,6 +32,24 @@ in {
       group = "netbox";
     };
     services = {
+      nginx = {
+        enable = true;
+        # user = "netbox";
+        recommendedTlsSettings = true;
+        # clientMaxBodySize = "25m";
+        virtualHosts."${config.networking.fqdn}" = {
+          locations = {
+            "/" = {
+              proxyPass = "http://[::1]:8001";
+              # proxyPass = "http://${config.services.netbox.listenAddress}:${config.services.netbox.port}";
+            };
+            "/static/" = {
+              alias = "${config.services.netbox.dataDir}/static/";
+            };
+          };
+          serverName = "${config.networking.fqdn}";
+        };
+      };
       netbox = {
         enable = true;
         package = pkgs.netbox;
