c8fcab220bf91aec9f17afbc2bc9e48b3397849c
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-06-12 18:48:28 +0200

    evolution

--- home-manager/profiles/main.nix
+++ home-manager/profiles/main.nix
@@ -90,7 +90,7 @@ with lib; {
       mprocs # tui for running multiple processes
       presenterm # presentation from terminal similar to reveal.js
       # linphone
-      # evolution
+      evolution
       nushell
       jan # ai
       qrtool
