2b245c72b38a297a8e1a4db7c7fc4a307e5c6dbf
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-07-29 22:03:13 +0200

    disable broken pdfmixtool

--- home-manager/modules/office/default.nix
+++ home-manager/modules/office/default.nix
@@ -3,14 +3,14 @@ with lib;
 let
   cfg = config.link.office;
   # tmp fix!
-  pdfmixtool = (import (builtins.fetchTarball {
-    url =
-      "https://github.com/NixOS/nixpkgs/archive/ea4c80b39be4c09702b0cb3b42eab59e2ba4f24b.tar.gz";
-    sha256 = "sha256:1xi53rlslcprybsvrmipm69ypd3g3hr7wkxvzc73ag8296yclyll";
-  }) {
-    system = "${pkgs.system}";
-    config.allowUnfree = true;
-  }).pdfmixtool;
+  # pdfmixtool = (import (builtins.fetchTarball {
+  #   url =
+  #     "https://github.com/NixOS/nixpkgs/archive/ea4c80b39be4c09702b0cb3b42eab59e2ba4f24b.tar.gz";
+  #   sha256 = "sha256:1xi53rlslcprybsvrmipm69ypd3g3hr7wkxvzc73ag8296yclyll";
+  # }) {
+  #   system = "${pkgs.system}";
+  #   config.allowUnfree = true;
+  # }).pdfmixtool;
 in {
   options.link.office.enable = mkEnableOption "activate office";
   config = mkIf cfg.enable {
