4dd1c00e7d78235295e8ddd5b46f7b9792da7705
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-09-02 03:38:34 +0200

    fix java

--- home-manager/modules/java/default.nix
+++ home-manager/modules/java/default.nix
@@ -4,14 +4,14 @@ let cfg = config.link.java;
 in {
   options.link.java.enable = mkEnableOption "activate java toolchain";
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [ jdk21 ];
+    # home.packages = with pkgs; [ jdk21 ];
     programs.vscode.profiles.default.extensions = with pkgs.vscode-extensions; [
       vscjava.vscode-java-pack
       redhat.java
       redhat.vscode-xml
     ];
-    programs.java.enable = true;
-    programs.vscode.userSettings."java.jdt.ls.jaca.home" =
-      "${pkgs.openjdk21}/lib/openjdk";
+    # programs.java.enable = true;
+    # programs.vscode.userSettings."java.jdt.ls.jaca.home" =
+    #   "${pkgs.openjdk21}/lib/openjdk";
   };
 }
