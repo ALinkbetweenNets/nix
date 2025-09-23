25cd53e01745860adeac1c21cd7e7f0461d3a18d
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-09-23 16:35:35 +0200

    go uses gcc

--- home-manager/modules/lang-go/default.nix
+++ home-manager/modules/lang-go/default.nix
@@ -4,7 +4,7 @@ let cfg = config.link.golang;
 in {
   options.link.golang.enable = mkEnableOption "activate golang toolchain";
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [ go delve ];
+    home.packages = with pkgs; [ go gcc delve ];
     programs.vscode.profiles.default.extensions = with pkgs.vscode-extensions;
       [ golang.go ];
   };
