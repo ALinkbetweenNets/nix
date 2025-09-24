f8af6d0aa182a5c0b7cbc75cecc07675d7f7b23a
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-09-24 22:37:43 +0200

    move displaylink to service

--- modules/hardware/laptop.nix
+++ modules/hardware/laptop.nix
@@ -4,41 +4,43 @@ let cfg = config.link.laptop;
 in {
   options.link.laptop = { enable = mkEnableOption "activate laptop"; };
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      wireless-regdb
-      (displaylink.overrideAttrs {
-        src = fetchurl {
-          sha256 =
-            "sha256-RJgVrX+Y8Nvz106Xh+W9N9uRLC2VO00fBJeS8vs7fKw=";
-          url =
-            "https://www.synaptics.com/sites/default/files/exe_files/2024-10/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu6.1-EXE.zip";
-        };
-      }) # .overrideAttrs
-    ];
+    environment.systemPackages = with pkgs;
+      [
+        wireless-regdb
+
+      ];
     boot.extraModulePackages = [ config.boot.kernelPackages.evdi ];
     boot.initrd.kernelModules = [ "evdi" ];
     environment.variables = { KWIN_DRM_PREFER_COLOR_DEPTH = "24"; };
-    # systemd.services.displaylink-server = {
-    #   enable = true;
-    #   # Ensure it starts after udev has done its work
-    #   requires = [ "systemd-udevd.service" ];
-    #   after = [ "systemd-udevd.service" ];
-    #   wantedBy = [ "multi-user.target" ]; # Start at boot
-    #   # *** THIS IS THE CRITICAL 'serviceConfig' BLOCK ***
-    #   serviceConfig = {
-    #     Type =
-    #       "simple"; # Or "forking" if it forks (simple is yecommon for daemons)
-    #     # The ExecStart path points to the DisplayLinkManager binary provided by the package
-    #     ExecStart = "${pkgs.displaylink}/bin/DisplayLinkManager";
-    #     # User and Group to run the service as (root is common for this type of daemon)
-    #     User = "root";
-    #     Group = "root";
-    #     # Environment variables that the service itself might need
-    #     # Environment = [ "DISPLAY=:0" ]; # Might be needed in some cases, but generally not for this
-    #     Restart = "on-failure";
-    #     RestartSec = 5; # Wait 5 seconds before restarting
-    #   };
-    # };
+    systemd.services.displaylink-server = {
+      enable = true;
+      # Ensure it starts after udev has done its work
+      requires = [ "systemd-udevd.service" ];
+      after = [ "systemd-udevd.service" ];
+      wantedBy = [ "multi-user.target" ]; # Start at boot
+      # *** THIS IS THE CRITICAL 'serviceConfig' BLOCK ***
+      serviceConfig = {
+        Type =
+          "simple"; # Or "forking" if it forks (simple is yecommon for daemons)
+        # The ExecStart path points to the DisplayLinkManager binary provided by the package
+        ExecStart = "${
+            (pkgs.displaylink.overrideAttrs {
+              src = pkgs.fetchurl {
+                sha256 = "sha256-RJgVrX+Y8Nvz106Xh+W9N9uRLC2VO00fBJeS8vs7fKw=";
+                url =
+                  "https://www.synaptics.com/sites/default/files/exe_files/2024-10/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu6.1-EXE.zip";
+              };
+            })
+          }/bin/DisplayLinkManager";
+        # User and Group to run the service as (root is common for this type of daemon)
+        User = "root";
+        Group = "root";
+        # Environment variables that the service itself might need
+        # Environment = [ "DISPLAY=:0" ]; # Might be needed in some cases, but generally not for this
+        Restart = "on-failure";
+        RestartSec = 5; # Wait 5 seconds before restarting
+      };
+    };
     link = {
       desktop.enable = true;
       hardware.enable = true;
