b9c1bd546c52b47d710cf97f3c353ffe9a69e563
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-09-24 22:12:33 +0200

    VGHTZBUJNK

--- modules/hardware/laptop.nix
+++ modules/hardware/laptop.nix
@@ -6,14 +6,14 @@ in {
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       wireless-regdb
-      # (displaylink.override {
-      #   src = fetchurl {
-      #     sha256 =
-      #       "sha256-RJgVrX+Y8Nvz106Xh+W9N9uRLC2VO00fBJeS8vs7fKw=";
-      #     url =
-      #       "https://www.synaptics.com/sites/default/files/exe_files/2024-10/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu6.1-EXE.zip";
-      #   };
-      # }) # .overrideAttrs
+      (displaylink.overrideAttrs {
+        src = fetchurl {
+          sha256 =
+            "sha256-RJgVrX+Y8Nvz106Xh+W9N9uRLC2VO00fBJeS8vs7fKw=";
+          url =
+            "https://www.synaptics.com/sites/default/files/exe_files/2024-10/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu6.1-EXE.zip";
+        };
+      }) # .overrideAttrs
     ];
     boot.extraModulePackages = [ config.boot.kernelPackages.evdi ];
     boot.initrd.kernelModules = [ "evdi" ];
