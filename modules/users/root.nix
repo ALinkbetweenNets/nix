30127374527810096e684cb9e7093ec132c59b14
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-11-25 13:30:00 +0100

    p4n public key for servers for backups

--- modules/users/root.nix
+++ modules/users/root.nix
@@ -9,7 +9,7 @@ in {
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOaLOyxsr6wgj0JoG/OrDywND2hG2nblOGUuZBPFG1U l@xn"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINI74luZ3xJcgaZYHzn5DtSpYufml+SbhZQV12gWGShS l@dn"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIj1OASl4OePBngiPlI4hixiD1GBlPOSoVNeoEcD23d+ l@fn"
-      ];
+      ]++lib.optionals ( config.link.server.enable ) ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINcTua6uOwlOQEUf+Jom0yYPuz0WSFJpVHJzu9E+B6EC root@p4n"];
       hashedPasswordFile =
         config.sops.secrets."users/root/hashedPassword".path; # Initial password
     };
