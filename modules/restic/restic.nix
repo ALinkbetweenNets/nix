b2d6e9cf2f79da8eb3974938e4bd0ac8de6070ab
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-11-27 17:27:50 +0100

    restic exclude dir defaults

--- modules/restic/restic.nix
+++ modules/restic/restic.nix
@@ -48,8 +48,9 @@ in {
     };
     backup-paths-exclude = mkOption {
       type = types.listOf types.str;
-      default = [ ];
-      example = [ "/home/link/.cache" ];
+      default =
+        [ ".cache" ".direnv" "result" "target" ".Trash-1000" "/var/cache" ];
+      example = [ "/home/l/.cache" ];
       description = "Paths to exclude from backup";
     };
   };
@@ -79,201 +80,199 @@ in {
     #   permissions = "u=rwx,g=,o=";
     #   capabilities = "cap_dac_read_search=+ep";
     # };
-    services.restic.backups =
-      let
-        # host = config.networking.hostName;
-        # script-post = host: site: ''
-        #   if [ $EXIT_STATUS -ne 0 ]; then
-        #     ${pkgs.curl}/bin/curl -u $NTFY_USER:$NTFY_PASS \
-        #     -H 'Title: Backup (${site}) on ${host} failed!' \
-        #     -H 'Tags: backup,restic,${host},${site}' \
-        #     -d "Restic (${site}) backup error on ${host}!" 'https://push.pablo.tools/link_backups'
-        #   else
-        #     ${pkgs.curl}/bin/curl -u $NTFY_USER:$NTFY_PASS \
-        #     -H 'Title: Backup (${site}) on ${host} successful!' \
-        #     -H 'Tags: backup,restic,${host},${site}' \
-        #     -d "Restic (${site}) backup success on ${host}!" 'https://push.pablo.tools/link_backups'
-        #   fi
-        # '';
-        restic-ignore-file = pkgs.writeTextFile {
-          name = "restic-ignore-file";
-          text = builtins.concatStringsSep "\n" cfg.backup-paths-exclude;
-        };
-      in
-      {
-        storagebox = mkIf (cfg.backup-paths-storagebox != [ ]) {
-          paths = cfg.backup-paths-storagebox;
-          repositoryFile =
-            config.sops.secrets."restic/storagebox/repository".path;
-          passwordFile = config.sops.secrets."restic/storagebox/password".path;
-          # environmentFile = "${config.sops.secrets."restic/backblaze-credentials".path}";
-          # backupCleanupCommand = script-post config.networking.hostName "storagebox";
-          pruneOpts = [
-            "--keep-daily 7"
-            "--keep-weekly 5"
-            "--keep-monthly 12"
-            "--keep-yearly 75"
-          ];
-          timerConfig = {
-            OnCalendar = "03:00";
-            Persistent = true;
-            RandomizedDelaySec = "5h";
-          };
-          extraBackupArgs = [
-            "--exclude-file=${restic-ignore-file}"
-            "--one-file-system"
-            "--compression=max"
-            # "--dry-run"
-            "-v"
-          ];
-          initialize = true;
-        };
-        lenny-storagebox = mkIf (cfg.backup-paths-lenny-storagebox != [ ]) {
-          paths = cfg.backup-paths-lenny-storagebox;
-          repositoryFile =
-            config.sops.secrets."restic/lenny-storagebox/repository".path;
-          passwordFile =
-            config.sops.secrets."restic/lenny-storagebox/password".path;
-          # environmentFile = "${config.sops.secrets."restic/backblaze-credentials".path}";
-          # backupCleanupCommand = script-post config.networking.hostName "storagebox";
-          pruneOpts = [
-            "--keep-daily 7"
-            "--keep-weekly 5"
-            "--keep-monthly 12"
-            "--keep-yearly 75"
-          ];
-          timerConfig = {
-            OnCalendar = "03:00";
-            Persistent = true;
-            RandomizedDelaySec = "5h";
-          };
-          extraBackupArgs = [
-            "--exclude-file=${restic-ignore-file}"
-            "--one-file-system"
-            "--compression=max"
-            # "--dry-run"
-            "-v"
-          ];
-          initialize = true;
-        };
-        sn = mkIf (cfg.backup-paths-sn != [ ]) {
-          paths = cfg.backup-paths-sn;
-          repositoryFile = config.sops.secrets."restic/sn/repository".path;
-          passwordFile = config.sops.secrets."restic/sn/password".path;
-          environmentFile = config.sops.secrets."restic/sn/environment".path;
-          pruneOpts = [
-            "--keep-daily 7"
-            "--keep-weekly 5"
-            "--keep-monthly 12"
-            "--keep-yearly 75"
-          ];
-          timerConfig = {
-            OnCalendar = "03:00";
-            Persistent = true;
-            RandomizedDelaySec = "5h";
-          };
-          extraBackupArgs = [
-            "--exclude-file=${restic-ignore-file}"
-            "--one-file-system"
-            "--compression=max"
-            # "--dry-run"
-            "-v"
-          ];
-          initialize = true;
-        };
-        p4n = mkIf (cfg.backup-paths-p4n != [ ]) {
-          paths = cfg.backup-paths-p4n;
-          repositoryFile = config.sops.secrets."restic/p4n/repository".path;
-          passwordFile = config.sops.secrets."restic/p4n/password".path;
-          # environmentFile = config.sops.secrets."restic/p4n/environment".path;
-          pruneOpts = [
-            "--keep-daily 7"
-            "--keep-weekly 5"
-            "--keep-monthly 12"
-            "--keep-yearly 75"
-          ];
-          timerConfig = {
-            OnCalendar = "03:00";
-            Persistent = true;
-            RandomizedDelaySec = "5h";
-          };
-          extraBackupArgs = [
-            "--exclude-file=${restic-ignore-file}"
-            "--one-file-system"
-            "--compression=max"
-            # "--dry-run"
-            "-v"
-          ];
-          initialize = true;
-        };
-        # sciebo = {
-        #   # Limit 30G
-        #   paths = cfg.backup-paths-sciebo;
-        #   # repositoryFile = config.sops.secrets."restic/sciebo/repository".path;
-        #   repository = "rclone:sciebo:restic";
-        #   passwordFile = config.sops.secrets."restic/sciebo/password".path;
-        #   # environmentFile = config.sops.secrets."restic/sciebo/environment".path;
-        #   rcloneConfigFile = config.sops.secrets."restic/sciebo/rclone".path;
-        #   # rcloneConfig = {
-        #   #   type = "webdav";
-        #   # };
-        #   pruneOpts = [
-        #     "--keep-daily 7"
-        #     "--keep-weekly 5"
-        #     "--keep-monthly 12"
-        #     "--keep-yearly 75"
-        #   ];
-        #   timerConfig = {
-        #     OnCalendar = "03:00";
-        #     Persistent = true;
-        #     RandomizedDelaySec = "5h";
-        #   };
-        #   extraBackupArgs = [
-        #     "--exclude-file = ${restic-ignore-file}"
-        #     " - -one-file-system "
-        #     " - -compression=max"
-        #     # "--dry-run"
-        #     "-v"
-        #   ];
-        #   initialize = true;
-        # };
-        onedrive = mkIf (cfg.backup-paths-onedrive != [ ]) {
-          paths = cfg.backup-paths-onedrive;
-          repositoryFile = config.sops.secrets."restic/onedrive/repository".path;
-          passwordFile = config.sops.secrets."restic/onedrive/password".path;
-          pruneOpts = [
-            "--keep-daily 7"
-            "--keep-weekly 5"
-            "--keep-monthly 12"
-            "--keep-yearly 75"
-          ];
-          timerConfig = {
-            OnCalendar = "03:00";
-            Persistent = true;
-            RandomizedDelaySec = "5h";
-          };
-          extraBackupArgs = [
-            "--exclude-file=${restic-ignore-file}"
-            "--one-file-system"
-            "--compression=max"
-            # "--dry-run"
-            "-v"
-          ];
-          initialize = true;
-        };
-        # s3-onsite = {
-        #   paths = cfg.backup-paths-onsite;
-        #   repository = "s3:https://vpn.s3.pablo.tools/restic";
-        #   environmentFile = "${config.sops.secrets."restic/credentials".path}";
-        #   passwordFile = "${config.sops.secrets."restic/repo-pw".path}";
-        #   backupCleanupCommand = script-post config.networking.hostName "NAS";
-        #   extraBackupArgs = [
-        #     "--exclude-file=${restic-ignore-file}"
-        #     "--one-file-system"
-        #     # "--dry-run"
-        #     "-vv"
-        #   ];
-        # };
+    services.restic.backups = let
+      # host = config.networking.hostName;
+      # script-post = host: site: ''
+      #   if [ $EXIT_STATUS -ne 0 ]; then
+      #     ${pkgs.curl}/bin/curl -u $NTFY_USER:$NTFY_PASS \
+      #     -H 'Title: Backup (${site}) on ${host} failed!' \
+      #     -H 'Tags: backup,restic,${host},${site}' \
+      #     -d "Restic (${site}) backup error on ${host}!" 'https://push.pablo.tools/link_backups'
+      #   else
+      #     ${pkgs.curl}/bin/curl -u $NTFY_USER:$NTFY_PASS \
+      #     -H 'Title: Backup (${site}) on ${host} successful!' \
+      #     -H 'Tags: backup,restic,${host},${site}' \
+      #     -d "Restic (${site}) backup success on ${host}!" 'https://push.pablo.tools/link_backups'
+      #   fi
+      # '';
+      restic-ignore-file = pkgs.writeTextFile {
+        name = "restic-ignore-file";
+        text = builtins.concatStringsSep "\n" cfg.backup-paths-exclude;
       };
+    in {
+      storagebox = mkIf (cfg.backup-paths-storagebox != [ ]) {
+        paths = cfg.backup-paths-storagebox;
+        repositoryFile =
+          config.sops.secrets."restic/storagebox/repository".path;
+        passwordFile = config.sops.secrets."restic/storagebox/password".path;
+        # environmentFile = "${config.sops.secrets."restic/backblaze-credentials".path}";
+        # backupCleanupCommand = script-post config.networking.hostName "storagebox";
+        pruneOpts = [
+          "--keep-daily 7"
+          "--keep-weekly 5"
+          "--keep-monthly 12"
+          "--keep-yearly 75"
+        ];
+        timerConfig = {
+          OnCalendar = "03:00";
+          Persistent = true;
+          RandomizedDelaySec = "5h";
+        };
+        extraBackupArgs = [
+          "--exclude-file=${restic-ignore-file}"
+          "--one-file-system"
+          "--compression=max"
+          # "--dry-run"
+          "-v"
+        ];
+        initialize = true;
+      };
+      lenny-storagebox = mkIf (cfg.backup-paths-lenny-storagebox != [ ]) {
+        paths = cfg.backup-paths-lenny-storagebox;
+        repositoryFile =
+          config.sops.secrets."restic/lenny-storagebox/repository".path;
+        passwordFile =
+          config.sops.secrets."restic/lenny-storagebox/password".path;
+        # environmentFile = "${config.sops.secrets."restic/backblaze-credentials".path}";
+        # backupCleanupCommand = script-post config.networking.hostName "storagebox";
+        pruneOpts = [
+          "--keep-daily 7"
+          "--keep-weekly 5"
+          "--keep-monthly 12"
+          "--keep-yearly 75"
+        ];
+        timerConfig = {
+          OnCalendar = "03:00";
+          Persistent = true;
+          RandomizedDelaySec = "5h";
+        };
+        extraBackupArgs = [
+          "--exclude-file=${restic-ignore-file}"
+          "--one-file-system"
+          "--compression=max"
+          # "--dry-run"
+          "-v"
+        ];
+        initialize = true;
+      };
+      sn = mkIf (cfg.backup-paths-sn != [ ]) {
+        paths = cfg.backup-paths-sn;
+        repositoryFile = config.sops.secrets."restic/sn/repository".path;
+        passwordFile = config.sops.secrets."restic/sn/password".path;
+        environmentFile = config.sops.secrets."restic/sn/environment".path;
+        pruneOpts = [
+          "--keep-daily 7"
+          "--keep-weekly 5"
+          "--keep-monthly 12"
+          "--keep-yearly 75"
+        ];
+        timerConfig = {
+          OnCalendar = "03:00";
+          Persistent = true;
+          RandomizedDelaySec = "5h";
+        };
+        extraBackupArgs = [
+          "--exclude-file=${restic-ignore-file}"
+          "--one-file-system"
+          "--compression=max"
+          # "--dry-run"
+          "-v"
+        ];
+        initialize = true;
+      };
+      p4n = mkIf (cfg.backup-paths-p4n != [ ]) {
+        paths = cfg.backup-paths-p4n;
+        repositoryFile = config.sops.secrets."restic/p4n/repository".path;
+        passwordFile = config.sops.secrets."restic/p4n/password".path;
+        # environmentFile = config.sops.secrets."restic/p4n/environment".path;
+        pruneOpts = [
+          "--keep-daily 7"
+          "--keep-weekly 5"
+          "--keep-monthly 12"
+          "--keep-yearly 75"
+        ];
+        timerConfig = {
+          OnCalendar = "03:00";
+          Persistent = true;
+          RandomizedDelaySec = "5h";
+        };
+        extraBackupArgs = [
+          "--exclude-file=${restic-ignore-file}"
+          "--one-file-system"
+          "--compression=max"
+          # "--dry-run"
+          "-v"
+        ];
+        initialize = true;
+      };
+      # sciebo = {
+      #   # Limit 30G
+      #   paths = cfg.backup-paths-sciebo;
+      #   # repositoryFile = config.sops.secrets."restic/sciebo/repository".path;
+      #   repository = "rclone:sciebo:restic";
+      #   passwordFile = config.sops.secrets."restic/sciebo/password".path;
+      #   # environmentFile = config.sops.secrets."restic/sciebo/environment".path;
+      #   rcloneConfigFile = config.sops.secrets."restic/sciebo/rclone".path;
+      #   # rcloneConfig = {
+      #   #   type = "webdav";
+      #   # };
+      #   pruneOpts = [
+      #     "--keep-daily 7"
+      #     "--keep-weekly 5"
+      #     "--keep-monthly 12"
+      #     "--keep-yearly 75"
+      #   ];
+      #   timerConfig = {
+      #     OnCalendar = "03:00";
+      #     Persistent = true;
+      #     RandomizedDelaySec = "5h";
+      #   };
+      #   extraBackupArgs = [
+      #     "--exclude-file = ${restic-ignore-file}"
+      #     " - -one-file-system "
+      #     " - -compression=max"
+      #     # "--dry-run"
+      #     "-v"
+      #   ];
+      #   initialize = true;
+      # };
+      onedrive = mkIf (cfg.backup-paths-onedrive != [ ]) {
+        paths = cfg.backup-paths-onedrive;
+        repositoryFile = config.sops.secrets."restic/onedrive/repository".path;
+        passwordFile = config.sops.secrets."restic/onedrive/password".path;
+        pruneOpts = [
+          "--keep-daily 7"
+          "--keep-weekly 5"
+          "--keep-monthly 12"
+          "--keep-yearly 75"
+        ];
+        timerConfig = {
+          OnCalendar = "03:00";
+          Persistent = true;
+          RandomizedDelaySec = "5h";
+        };
+        extraBackupArgs = [
+          "--exclude-file=${restic-ignore-file}"
+          "--one-file-system"
+          "--compression=max"
+          # "--dry-run"
+          "-v"
+        ];
+        initialize = true;
+      };
+      # s3-onsite = {
+      #   paths = cfg.backup-paths-onsite;
+      #   repository = "s3:https://vpn.s3.pablo.tools/restic";
+      #   environmentFile = "${config.sops.secrets."restic/credentials".path}";
+      #   passwordFile = "${config.sops.secrets."restic/repo-pw".path}";
+      #   backupCleanupCommand = script-post config.networking.hostName "NAS";
+      #   extraBackupArgs = [
+      #     "--exclude-file=${restic-ignore-file}"
+      #     "--one-file-system"
+      #     # "--dry-run"
+      #     "-vv"
+      #   ];
+      # };
+    };
   };
 }
