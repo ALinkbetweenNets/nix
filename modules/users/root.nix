6095640a4326da450097ea2c18daeb903102df7c
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2025-12-06 16:58:14 +0100

    formatting

--- modules/users/root.nix
+++ modules/users/root.nix
@@ -7,9 +7,11 @@ in {
     users.users.root = {
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOaLOyxsr6wgj0JoG/OrDywND2hG2nblOGUuZBPFG1U l@xn"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINI74luZ3xJcgaZYHzn5DtSpYufml+SbhZQV12gWGShS l@dn"
+        # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINI74luZ3xJcgaZYHzn5DtSpYufml+SbhZQV12gWGShS l@dn"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIj1OASl4OePBngiPlI4hixiD1GBlPOSoVNeoEcD23d+ l@fn"
-      ]++lib.optionals ( config.link.server.enable ) ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINcTua6uOwlOQEUf+Jom0yYPuz0WSFJpVHJzu9E+B6EC root@p4n"];
+      ] ++ lib.optionals (config.link.server.enable) [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINcTua6uOwlOQEUf+Jom0yYPuz0WSFJpVHJzu9E+B6EC root@p4n"
+      ];
       hashedPasswordFile =
         config.sops.secrets."users/root/hashedPassword".path; # Initial password
     };
