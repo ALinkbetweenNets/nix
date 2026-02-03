4212cb7b65927589c28f102aeb2ca8424d6aa63c
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2026-02-03 14:39:00 +0100

    curl alias

--- home-manager/modules/shell/default.nix
+++ home-manager/modules/shell/default.nix
@@ -115,6 +115,7 @@ in {
             (ps: with ps; [ rustup zlib openssl.dev pkg-config ]);
         in
         {
+          crl="curl -A \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:145.0) Gecko/20100101 Firefox/145.0\" -vk";
           htt = "httpx -nc -sc -fr -location -td -rt -cdn -title -v -rl 5000 -timeout 60 -tls-probe -csp-probe -tls-grab -pipeline -http2 -vhost -pa -rl 10 -t 8 -ss -system-chrome -sid 5s -probe -cname -ip -extract-fqdn -ws -method -lc -jarm -favicon md5 -ct -c -p 'http:80,8080,https:443,8443' -mc 400";
           dns = "dnsx -re -all -nc -cdn -e soa";
           rep = "/home/l/s/repeat.sh";
