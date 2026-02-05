e5e8e1b214a550b16b04e9d4d05d5402f85aa6b6
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2026-02-05 23:37:48 +0100

    libvirt open 53 67

--- modules/libvirt/default.nix
+++ modules/libvirt/default.nix
@@ -1,10 +1,23 @@
-{ config, system-config, pkgs, lib, ... }:
+{
+  config,
+  system-config,
+  pkgs,
+  lib,
+  ...
+}:
 with lib;
-let cfg = config.link.libvirt;
-in {
+let
+  cfg = config.link.libvirt;
+in
+{
   options.link.libvirt.enable = mkEnableOption "activate libvirt";
   config = mkIf cfg.enable {
-    boot.kernelModules = [ "vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio" ];
+    boot.kernelModules = [
+      "vfio_virqfd"
+      "vfio_pci"
+      "vfio_iommu_type1"
+      "vfio"
+    ];
     environment.systemPackages = with pkgs; [
       virtiofsd
       libvirt
@@ -37,10 +50,28 @@ in {
     #   "f /dev/shm/looking-glass 0660 alex qemu-libvirtd -"
     # ];
     programs.dconf.enable = lib.mkForce true;
-    # networking.bridges.br0.interfaces = [ config.link.eth ];
-    # networking.interfaces.br0 = { useDHCP = true; };
-    networking.firewall.allowedTCPPorts = [
-      5900 # spice
-    ];
+    networking = {
+      # networking.bridges.br0.interfaces = [ config.link.eth ];
+      # networking.interfaces.br0 = { useDHCP = true; };
+      nat = {
+        enable = true;
+        internalInterfaces = [ "virbr0" ];
+      };
+      # interfaces.virbr0 = {
+      #   ipv4.addresses = [
+      #     {
+      #       address = "192.168.122.1";
+      #       prefixLength = 24;
+      #     }
+      #   ];
+      # };
+      firewall.interfaces.virbr0.allowedUDPPorts = [
+        53 # DNS
+        67 # DHCP
+      ];
+      firewall.allowedTCPPorts = [
+        5900 # spice
+      ];
+    };
   };
 }
