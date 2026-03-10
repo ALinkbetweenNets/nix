6f67093b592da077721e5346396ce52f55ec983c
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2026-03-10 13:46:42 +0100

    nginx secure ciphers, fix headers

--- modules/services/nginx.nix
+++ modules/services/nginx.nix
@@ -1,10 +1,21 @@
-{ config, system-config, pkgs, lib, ... }:
+{
+  config,
+  system-config,
+  pkgs,
+  lib,
+  ...
+}:
 with lib;
-let cfg = config.link.nginx;
-in {
+let
+  cfg = config.link.nginx;
+in
+{
   options.link.nginx.enable = mkEnableOption "activate nginx";
   config = mkIf cfg.enable {
-    networking.firewall.allowedUDPPorts = [ 80 443 ];
+    networking.firewall.allowedUDPPorts = [
+      80
+      443
+    ];
     networking.firewall.allowedTCPPorts = [
       # 25
       80
@@ -21,7 +32,7 @@ in {
     sops.secrets."cloudflare-api" = { };
     security.acme = {
       acceptTerms = true;
-      defaults.email = "link2502+acme" + "@"+"proton.me";
+      defaults.email = "link2502+acme" + "@" + "proton.me";
       defaults.webroot = "/var/lib/acme/acme-challenge";
       certs."${config.link.domain}" = {
         domain = config.link.domain;
@@ -48,18 +59,24 @@ in {
       logError = "stderr debug";
       enableQuicBPF = true;
       package = pkgs.nginx.override {
-        openssl = pkgs.libressl;
+        #openssl = pkgs.libressl;
         modules = with pkgs.nginxModules; [ geoip2 ]; # echo
         buildInputs = oldAttrs.buildInputs ++ [ pkgs.libmaxminddb ];
       };
       clientMaxBodySize = "6000m";
+      sslProtocols = "TLSv1.3";
+      # sslCiphers = "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:DHE-RSA-CHACHA20-POLY1305:@SECLEVEL=2"; # Mozilla recommendation
       commonHttpConfig = ''
-        # sslCiphers = "AES256+EECDH:AES256+EDH:!aNULL";
-        # ssl_protocols TLSv1.3;
+        # ssl_ciphers = "AES256+EECDH:AES256+EDH:!aNULL@SECLEVEL=2";
+        ssl_ecdh_curve X25519:prime256v1:secp384r1;
+        # ssl_protocols TLSv1.2 TLSv1.3;
+        # ssl_ciphers HIGH:!aNULL:!MD5:@SECLEVEL=2;
         # ssl_conf_command Options KTLS; # not supported on nc
         # ssl_conf_command Groups "X25519MLKEM768:X25519:P-256:P-384"
         ssl_session_timeout 1d;
-        ssl_session_cache shared:SSL:10m;
+        ssl_session_cache shared:SSL:10m; # 40000 sessions
+        # cache_max_size 1g;
+        # cache_valid 200 30m;
         # Breaks forward secrecy: https://github.com/mozilla/server-side-tls/issues/135
         ssl_session_tickets off;
         # We don't enable insecure ciphers by default, so this allows
@@ -74,35 +91,44 @@ in {
         #   '"$request" $status $body_bytes_sent '
         #   '"$http_referer" "$http_user_agent"'
         #   ' -- "$request_body"';
-        # Add HSTS header with preloading to HTTPS requests.
         # Adding this header to HTTP requests is discouraged
         index index.php index.html /index.php$request_uri;
         server_names_hash_bucket_size 128;
         proxy_headers_hash_max_size 1024;
         proxy_headers_hash_bucket_size 256;
+        # keepalive_max_robots 100;
+        # keepalive_timeout 65;
+        # Add HSTS header with preloading to HTTPS requests.
         add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" always;
         # proxy_hide_header Strict-Transport-Security;
-        # ssl_stapling_verify on;
-        proxy_hide_header Content-Security-Policy;
-        add_header Content-Security-Policy "frame-ancestors 'self' resource: blob: https://*.alinkbetweennets.de ws://*.alinkbetweennets.de ; object-src 'none'; default-src 'self' 'unsafe-inline' 'unsafe-eval' resource: data: blob: https://*.alinkbetweennets.de ws://*.alinkbetweennets.de https://tiles.immich.cloud https://static.immich.cloud https://api-l.cofractal.com https://maputnik.github.io https://fonts.openmaptiles.org https://fonts.googleapis.com ; script-src 'self' 'unsafe-eval' 'unsafe-inline' resource: blob: https://*.alinkbetweennets.de ws://*.alinkbetweennets.de ; script-src-elem 'self' 'unsafe-inline' resource: blob: https://*.alinkbetweennets.de ws://*.alinkbetweennets.de ; font-src https://fonts.openmaptiles.org https://fonts.googleapis.com https://fonts.gstatic.com 'self' 'unsafe-inline' 'unsafe-eval' data: blob: ; img-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob: https://secure.gravatar.com https://videos.owncast.online https://www.gravatar.com https://logo.clearbit.com http://*.alinkbetweennets.de https://*.alinkbetweennets.de ws://*.alinkbetweennets.de ; frame-src 'self' 'unsafe-inline' https://*.alinkbetweennets.de ws://*.alinkbetweennets.de https://www.youtube.com https://youtube.com https://www.youtu.be ; media-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob: https://assets.owncast.tv https://videos.owncast.online https://www.gravatar.com https://logo.clearbit.com https://*.alinkbetweennets.de ws://*.alinkbetweennets.de ; base-uri 'self' *.alinkbetweennets.de alinkbetweennets.de;" always;
+        ssl_stapling on;
+        ssl_stapling_verify on;
+        resolver 8.8.8.8;
+        # proxy_hide_header Content-Security-Policy; # maybe remove this
+        add_header Content-Security-Policy "frame-ancestors 'self' resource: blob: https://*.alinkbetweennets.de ws://*.alinkbetweennets.de ; object-src 'none'; default-src 'self' 'unsafe-inline' 'unsafe-eval' resource: data: blob: https://*.alinkbetweennets.de ws://*.alinkbetweennets.de https://tiles.immich.cloud https://static.immich.cloud https://api-l.cofractal.com https://maputnik.github.io https://fonts.openmaptiles.org https://fonts.googleapis.com ; script-src 'self' 'unsafe-eval' 'unsafe-inline' resource: blob: https://*.alinkbetweennets.de ws://*.alinkbetweennets.de 'sha256-ZswfTY7H35rbv8WC7NXBoiC7WNu86vSzCDChNWwZZDM=' 'sha256-Y1z0SyVx5S7ywM8HEuxVVkmBpqUjj6qZLBNBVk8E6h8=' 'sha256-UOoHSE7EadmP3+ngOCGeSjlMRRUHEmkJYHp72XcJwMA=' 'sha256-h5wSYKWbmHcoYTdkHNNguMswVNCphpvwW+uxooXhF/Y='; script-src-elem 'self' 'unsafe-inline' resource: blob: https://*.alinkbetweennets.de ws://*.alinkbetweennets.de 'sha256-ZswfTY7H35rbv8WC7NXBoiC7WNu86vSzCDChNWwZZDM=' 'sha256-Y1z0SyVx5S7ywM8HEuxVVkmBpqUjj6qZLBNBVk8E6h8=' 'sha256-UOoHSE7EadmP3+ngOCGeSjlMRRUHEmkJYHp72XcJwMA=' 'sha256-h5wSYKWbmHcoYTdkHNNguMswVNCphpvwW+uxooXhF/Y=' 'sha256-Y1z0SyVx5S7ywM8HEuxVVkmBpqUjj6qZLBNBVk8E6h8=' 'sha256-ZswfTY7H35rbv8WC7NXBoiC7WNu86vSzCDChNWwZZDM=' 'sha256-OGjU0xQxuhdJDf8iMcgZXmRJZD2QZBQJRLwrFlOBLkI=' 'sha256-ZswfTY7H35rbv8WC7NXBoiC7WNu86vSzCDChNWwZZDM=' 'sha256-OGjU0xQxuhdJDf8iMcgZXmRJZD2QZBQJRLwrFlOBLkI=' 'sha256-R5+O+GguAMEaEziO1n5XU7JdxbmNH9PrD0455tGIo+Y=' 'sha256-R/9llE0s4++pxJWFqpVdB+eQJk2ANaxrUkOoEgbf4BE=' 'sha256-+ac8khtNoT0aNZRgSgtufqMcKat1zoMcy2QKOy1king=' 'sha256-OEAthcFABIfn7TlCbTV6bGAv1qO5kVS02Tbru9uBM7E=' 'sha256-550mecyv+58pd1gBLH1QBIOUYSFa6dRGnPr5EQydW1Y=' 'sha256-XqyszejV2KjHp1R/rjEZrlZr+jaJpoSSF34CliTkwsA=' 'sha256-EzOdKxNif8wH7zIpBWq8C8s7ReXFFUgKIeh3S8mbzmg=' 'sha256-fWlVIwe2LsO09S7H48jPEdZpXqvWNASN9MJtP9UqorQ='; font-src https://fonts.openmaptiles.org https://fonts.googleapis.com https://fonts.gstatic.com 'self' 'unsafe-inline' 'unsafe-eval' data: blob: ; img-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob: https://secure.gravatar.com https://videos.owncast.online https://www.gravatar.com https://logo.clearbit.com http://*.alinkbetweennets.de https://*.alinkbetweennets.de ws://*.alinkbetweennets.de ; frame-src 'self' 'unsafe-inline' https://*.alinkbetweennets.de ws://*.alinkbetweennets.de https://www.youtube.com https://youtube.com https://www.youtu.be ; media-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob: https://assets.owncast.tv https://videos.owncast.online https://www.gravatar.com https://logo.clearbit.com https://*.alinkbetweennets.de ws://*.alinkbetweennets.de ; base-uri 'self' *.alinkbetweennets.de alinkbetweennets.de;" always;
+        # for inside csp: report-uri http://v2202312204123249185.ultrasrv.de:4000; report-to default
+        # add_header Report-To '{"max_age":31536000,"endpoints":[{"url":"http://v2202312204123249185.ultrasrv.de:4000/csp-report"}]}';
         # no-referrer
         add_header Referrer-Policy strict-origin;
         # proxy_hide_header X-Frame-Options;
         add_header X-Frame-Options sameorigin;
+        # add_header X-Frame-Options DENY;
         # Prevent injection of code in other mime types (XSS Attacks)
         # proxy_hide_header X-Content-Type-Options;
-        # add_header X-Content-Type-Options nosniff;
+        add_header X-Content-Type-Options nosniff always;
         # This might create errors - doc
         # Yep it destroyed the immich login - me
         #proxy_cookie_path / "/; secure; HttpOnly; SameSite=strict";
-        add_header X-Real-IP $remote_addr;
-        add_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        add_header X-Forwarded-Proto https;
-        add_header X-Forwarded-Ssl on;
-        add_header X-Forwarded-Port $server_port;
-        add_header X-Forwarded-Host $host;
-        add_header Host $http_host;
-        add_header origin $http_origin;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto https;
+        proxy_set_header X-Forwarded-Ssl on;
+        proxy_set_header X-Forwarded-Port $server_port;
+        proxy_set_header X-Forwarded-Host $host;
+        proxy_set_header Host $host;
+        proxy_set_header origin $http_origin;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
         add_header X-XSRF-TOKEN $http_x_xsrf_token;
         # proxy_connect_timeout 300;
         # Default is HTTP/1, keepalive is only enabled in HTTP/1.1
