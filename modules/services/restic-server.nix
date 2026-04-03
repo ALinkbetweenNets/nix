75052726658e66b7bb9a7479c9e09239739e02a9
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2026-04-03 17:28:08 +0200

    restic rest server only on tailscale

--- modules/services/restic-server.nix
+++ modules/services/restic-server.nix
@@ -1,7 +1,15 @@
-{ config, system-config, pkgs, lib, ... }:
+{
+  config,
+  system-config,
+  pkgs,
+  lib,
+  ...
+}:
 with lib;
-let cfg = config.link.services.restic-server;
-in {
+let
+  cfg = config.link.services.restic-server;
+in
+{
   options.link.services.restic-server = {
     enable = mkEnableOption "activate restic-server";
     expose = mkOption {
@@ -17,8 +25,7 @@ in {
     nginx = mkOption {
       type = types.bool;
       default = config.link.nginx.enable;
-      description =
-        "expose the application to the internet with NGINX and ACME";
+      description = "expose the application to the internet with NGINX and ACME";
     };
     port = mkOption {
       type = types.int;
@@ -34,16 +41,15 @@ in {
         prometheus = false;
         privateRepos = true;
         listenAddress =
-          if cfg.expose-port then
-            "0.0.0.0:${toString cfg.port}"
-          else
-            "127.0.0.1:${toString cfg.port}";
+          if cfg.expose-port then "0.0.0.0:${toString cfg.port}" else "127.0.0.1:${toString cfg.port}";
         appendOnly = true;
       };
       nginx.virtualHosts."restic.${config.link.domain}" = mkIf cfg.nginx {
         enableACME = true;
         forceSSL = true;
-        locations."/" = { proxyPass = "http://127.0.0.1:2500"; };
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:2500";
+        };
         extraConfig = mkIf (!cfg.expose) ''
           allow ${config.link.service-ip}/24;
           allow 127.0.0.1;
@@ -51,5 +57,6 @@ in {
         '';
       };
     };
+    networking.firewall.interfaces."tailscale0".allowedTCPPorts = mkIf cfg.expose-port [ cfg.port ];
   };
 }
