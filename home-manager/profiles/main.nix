0f6b193e0cf6c4764300f250001878f67839cb6a
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2026-04-17 09:59:28 +0200

    zotero

--- home-manager/profiles/main.nix
+++ home-manager/profiles/main.nix
@@ -88,6 +88,7 @@ with lib; {
     home.packages = with pkgs; [
       # bitwarden-cli
       pympress
+      zotero
       openai-whisper # transcription
       ripgrep-all # also search pdfs, ebooks, office docs, zip, tar.gz
       kdePackages.plasma-browser-integration
